fc6f27412d42ac7d7f0477f260c48e0312d70824
TrivediMamta <mtrivedi@adobe.com>
2021-07-06 15:11:26 +0530

    Added site for sampling.

--- ui.frontend/clientlib.config.js
+++ ui.frontend/clientlib.config.js
@@ -42,10 +42,57 @@ module.exports = {
   context: BUILD_DIR,
   clientLibRoot: CLIENTLIB_DIR,
   libs: [
+  {
+        ...libsBaseConfig,
+        name: 'clientlib-dependencies',
+        categories: ['etisalat.dependencies'],
+        assets: {
+          // Copy entrypoint scripts and stylesheets into the respective ClientLib
+          // directories
+          js: {
+            cwd: 'clientlib-dependencies',
+            files: ['**/*.js'],
+            flatten: false
+          },
+          css: {
+            cwd: 'clientlib-dependencies',
+            files: ['**/*.css'],
+            flatten: false
+          }
+        }
+      },
+  {
+        ...libsBaseConfig,
+        name: 'clientlib-site',
+        categories: ['etisalat.site'],
+      dependencies: ['etisalat.dependencies'],
+        assets: {
+          // Copy entrypoint scripts and stylesheets into the respective ClientLib
+          // directories
+          js: {
+            cwd: 'clientlib-site',
+            files: ['**/*.js'],
+            flatten: false
+          },
+          css: {
+            cwd: 'clientlib-site',
+            files: ['**/*.css'],
+            flatten: false
+          },
+          // Copy all other files into the `resources` ClientLib directory
+                    resources: {
+                      cwd: 'clientlib-site',
+                      files: ['**/*.*'],
+                      flatten: false,
+                      ignore: ['**/*.js', '**/*.css']
+                    }
+                  }
+                },
     {
       ...libsBaseConfig,
       name: 'clientlib-hiuapp',
       categories: ['etisalat.hiuapp'],
+      dependencies: ['etisalat.dependencies'],
       assets: {
         // Copy entrypoint scripts and stylesheets into the respective ClientLib
         // directories
@@ -73,6 +120,7 @@ module.exports = {
           ...libsBaseConfig,
           name: 'clientlib-fivemobile',
           categories: ['etisalat.fivemobile'],
+      dependencies: ['etisalat.dependencies'],
           assets: {
             // Copy entrypoint scripts and stylesheets into the respective ClientLib
             // directories
--- ui.frontend/webpack.common.js
+++ ui.frontend/webpack.common.js
@@ -16,6 +16,7 @@ const resolve = {
 module.exports = {
     resolve: resolve,
     entry: {
+        site: SOURCE_ROOT + '/site/main.js' ,
         fivemobile: SOURCE_ROOT + '/fivemobile/main.js' ,
         hiuapp: SOURCE_ROOT + '/hiuapp/main.js'
     },
@@ -105,6 +106,7 @@ module.exports = {
             filename: 'clientlib-[name]/[name].css'
         }),
         new CopyWebpackPlugin([
+            { from: path.resolve(__dirname, SOURCE_ROOT + '/site/resources'), to: './clientlib-site/' },
             { from: path.resolve(__dirname, SOURCE_ROOT + '/fivemobile/resources'), to: './clientlib-fivemobile/' },
             { from: path.resolve(__dirname, SOURCE_ROOT + '/hiuapp/resources'), to: './clientlib-hiuapp/' }
         ])
