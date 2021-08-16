7a9e9a1ec3912400084b9eb3f65db5e41defd080
webXsatya <satyajeet.dhere@capgemini.com>
2021-08-17 02:15:01 +0530

    ETSL-73

--- ui.frontend/src/main/webpack/etisalat/site.js
+++ ui.frontend/src/main/webpack/etisalat/site.js
@@ -1,10 +1,27 @@
-$('.hamburger').on('click', function (){
+$('.hamburger').on('click', function () {
     $(".hamburger").toggleClass("is-active");
     $(".main-menu-mobile").toggleClass("mob-visible");
 });
 
 // Dyanamic year 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.copyright-year').text(new Date().getFullYear());
-  });
-  
+});
+
+
+$(document).ready(function () {
+    $(".beigebg-wrapper").hide();
+    $(".cmp-teaser__action-link").addClass("sort_asc");
+
+    $(".cmp-teaser__action-link").on("click", function () {
+
+        var txt = $(".beigebg-wrapper").is(':visible') ? 'VIEW MORE' : 'VIEW LESS';
+
+        $(".cmp-teaser__action-link").text(txt);
+
+        $('.cmp-teaser__action-link').toggleClass('sort_asc sort_desc');
+
+        $('.beigebg-wrapper').slideToggle();
+
+    });
+});
