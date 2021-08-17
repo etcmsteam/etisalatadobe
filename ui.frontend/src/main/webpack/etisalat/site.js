1489d4817a7f98cdd00cf0135ba590a38dd3f371
webXsatya <satyajeet.dhere@capgemini.com>
2021-08-17 10:20:48 +0530

    JS code changes

--- ui.frontend/src/main/webpack/etisalat/site.js
+++ ui.frontend/src/main/webpack/etisalat/site.js
@@ -3,25 +3,18 @@ $('.hamburger').on('click', function () {
     $(".main-menu-mobile").toggleClass("mob-visible");
 });
 
-// Dyanamic year 
+
 $(document).ready(function () {
+    // Dyanamic year 
     $('.copyright-year').text(new Date().getFullYear());
-});
 
-
-$(document).ready(function () {
+    //ETSL -38
     $(".beigebg-wrapper").hide();
     $(".cmp-teaser__action-link").addClass("sort_asc");
-
     $(".cmp-teaser__action-link").on("click", function () {
-
         var txt = $(".beigebg-wrapper").is(':visible') ? 'VIEW MORE' : 'VIEW LESS';
-
         $(".cmp-teaser__action-link").text(txt);
-
         $('.cmp-teaser__action-link').toggleClass('sort_asc sort_desc');
-
         $('.beigebg-wrapper').slideToggle();
-
     });
 });
