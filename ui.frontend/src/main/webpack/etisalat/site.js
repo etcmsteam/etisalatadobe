eb10d162114110ba78c2be7efa9d61d645f96445
hcl77000 <hcl77000@adobe.com>
2021-08-18 00:34:37 +0530

    FE styles+js added, aem dialog+html modified, clientlib js removed ,

--- ui.frontend/src/main/webpack/etisalat/site.js
+++ ui.frontend/src/main/webpack/etisalat/site.js
@@ -7,4 +7,37 @@ $('.hamburger').on('click', function (){
 $(document).ready(function(){
     $('.copyright-year').text(new Date().getFullYear());
   });
-  
+
+// Nested Accordion View All, View Less Script
+$(".accordion-view").click(function() {
+    if (!$(".more").hasClass('hidden') &&
+        $(".less").hasClass('hidden')) {
+        $(".cmp-viewall").children().each(function(index) {
+            var numberOfItems = $('.cmp-viewall').attr('data-items-show');
+            if ($(this).hasClass('hidden')) {
+                $(this).addClass('show');
+                $(this).removeClass('hidden');
+                $(".less").removeClass('hidden');
+                $(".more").addClass('hidden');
+            }
+        });
+
+
+
+    } else if ($(".more").hasClass('hidden') &&
+        !$(".less").hasClass('hidden')) {
+        $(".cmp-viewall").children().each(function(index) {
+            var numberOfItems = $('.cmp-viewall').attr('data-items-show');
+            if ((index + 1) > numberOfItems) {
+                $(this).addClass('hidden');
+                $(this).removeClass('show');
+                $(".accordion-view").addClass('show');
+                $(".accordion-view").removeClass('hidden');
+                $(".more").removeClass('hidden');
+                $(".less").addClass('hidden');
+            }
+        });
+
+    }
+
+});
