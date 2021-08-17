519d0182a896b348772eeba1ad65366e4586ed2b
webXsatya <satyajeet.dhere@capgemini.com>
2021-08-17 20:12:16 +0530

    ETSl - 73 - Review comment resolved

--- ui.frontend/src/main/webpack/etisalat/site.js
+++ ui.frontend/src/main/webpack/etisalat/site.js
@@ -1,20 +1,23 @@
-$('.hamburger').on('click', function () {
-    $(".hamburger").toggleClass("is-active");
-    $(".main-menu-mobile").toggleClass("mob-visible");
-});
-
-
 $(document).ready(function () {
+
+    $('.hamburger').on('click', function () {
+        $(".hamburger").toggleClass("is-active");
+        $(".main-menu-mobile").toggleClass("mob-visible");
+    });
+
+
+
     // Dyanamic year 
     $('.copyright-year').text(new Date().getFullYear());
 
     //ETSL -38
     $(".beigebg-wrapper").hide();
-    $(".cmp-teaser__action-link").addClass("sort_asc");
-    $(".cmp-teaser__action-link").on("click", function () {
-        var txt = $(".beigebg-wrapper").is(':visible') ? 'VIEW MORE' : 'VIEW LESS';
-        $(".cmp-teaser__action-link").text(txt);
-        $('.cmp-teaser__action-link').toggleClass('sort_asc sort_desc');
+    $(".teaser-viewall").addClass("sort_asc");
+    $(".teaser-viewall").on("click", function () {
+        $(this).find('span').toggleClass('hidden');
+        $(this).toggleClass(function(){
+            return $(this).is('.sort_asc, .sort_desc') ? 'sort_asc sort_desc' : 'red';
+          })
         $('.beigebg-wrapper').slideToggle();
     });
 });
