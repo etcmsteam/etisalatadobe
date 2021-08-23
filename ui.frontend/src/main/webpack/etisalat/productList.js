7ae1e29cf087e3395eedae298ba40f57055411b5
Sundar N <cap08550@adobe.com>
2021-08-23 16:50:30 +0530

    code fixed for grid sizing

--- ui.frontend/src/main/webpack/etisalat/productList.js
+++ ui.frontend/src/main/webpack/etisalat/productList.js
@@ -1842,7 +1842,7 @@ function getProductCard(data) {
     var product = products[i];   
    
     if (featuredPosition == i + 1) {
-      html += '<div class="swiper-slide col-xs-12 col-sm-6 col-md-4" style="width: 306px;">';
+      html += '<div class="swiper-slide col-xs-12 col-sm-6 col-md-4" style="width: 306px;min-width:306px">';
       html += featuredTile.html();
       html += '</div>'
     }
