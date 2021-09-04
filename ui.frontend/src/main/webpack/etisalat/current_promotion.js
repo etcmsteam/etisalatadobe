48d48ef79406630affbc58f852f5628f2d90a94e
webXsatya <satyajeet.dhere@capgemini.com>
2021-09-05 04:13:37 +0530

    ETSL-36-Development-Done

---
+++ ui.frontend/src/main/webpack/etisalat/current_promotion.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+    var initSwiper;
+    initSwiper = function () {
+        // plans table slider for CMS modules start
+        $(document).find('.current-promotions-wrraper').each(function (index) {
+            $(this).addClass('c-p-slider' + index);
+            var $slider = $(this);
+            $slider.find('.next').addClass('right' + index);
+            $slider.find('.prev').addClass('left' + index);
+            var $carouselSliderCurrentPromotions = new Swiper('.c-p-slider' + index + ' .swiper-container', {
+                //clickable: true,
+                scrollbar: $(this).find('.scrollbar'),
+                nextButton: '.next.right' + index,
+                prevButton: '.prev.left' + index,
+                scrollbarHide: false,
+                scrollbarDraggable: true,
+                breakpoints: {
+                    540: {
+
+                        spaceBetween: 16,
+                        slidesPerView: 1.68
+                    },
+                    768: {
+                        spaceBetween: 16,
+                        slidesPerView: 2.5
+                    },
+                    1024: {
+                        spaceBetween: 24,
+                        slidesPerView: 3
+                    },
+                    1440: {
+                        spaceBetween: 24,
+                        slidesPerView: 3
+                    },
+                    9999: {
+                        spaceBetween: 24,
+                        centeredSlides: false,
+                        slidesPerView: 3
+                    }
+                }
+            });
+        });
+        // plans table slider for CMS modules ends
+    }
+    // register the event handlers
+    initSwiper();
+    // popup 
+
+    $(".learn-more-wrap a").on("click", function(event) {
+        var dataLabel = $(this).attr("data-label");
+        if (dataLabel) {
+            $('#' + dataLabel).addClass('show');
+            $('#' + dataLabel).removeClass('fade');
+            $('body').addClass('freeze');
+            $(".promo-offers-popup-wrapper").show;
+            $(".promo-offers-popup-wrapper").css('display', 'block');
+            $('.modalpopup').addClass('show');
+            $(".promo-offers-popup-wrapper").append($('#' + dataLabel));
+        }
+    });
+    
+    // close popup
+    var closePopUp;
+    closePopUp = function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+        var currentOpendPopUp = $(this).closest('.promo-offers-popup-wrapper');
+        $(currentOpendPopUp).removeClass('show');
+        $(currentOpendPopUp).css('display', 'none');
+        $('body').removeClass('freeze');
+    };
+    $('.promo-offers-popup-wrapper').off('click').on('click', '.nv-modal-close', closePopUp);
+});
