217d24d810c0d2b005e5203a5682730886e771ec
Ramya <cap40317@adobe.com>
2021-09-17 19:16:22 +0530

    slider js with breakpoints&css

--- ui.frontend/src/main/webpack/ewallet/ewallet-mobile-slider.js
+++ ui.frontend/src/main/webpack/ewallet/ewallet-mobile-slider.js
@@ -1,58 +1,31 @@
-$(document).ready(function () {
-    function setSpacebetweenContextNavCarousel() {
-      var spaceBetween = 0;
-      $(document).find('.slider-outlet-brands ').each(function () {
-        var slideTemp = $(this)[0].swiper;
-        if (slideTemp) {
-          spaceBetween = ($(this).width() - ($(this).width() / 10 * 3 * 3)) / 2;
-          slideTemp.params.spaceBetween = spaceBetween;
-          $(this).find('.swiper-wrapper').removeAttr('style');
-          $(this).find('.swiper-slide').removeAttr('style');
-          slideTemp.update(true);
-        }
-      });
-  
+
+
+var swiper = new Swiper('.slider-outlet-brands', {
+  autoplay: false,
+  slidesPerView: 8,
+  slidesPerColumn: 2,
+  simulateTouch: true,
+  noSwiping: true,
+  noSwipingClass: 'no-swap-desktop',
+ 
+  breakpoints: {
+    1024: {
+      slidesPerView: 6,
+      slidesPerColumn: 2,
+      spaceBetweenSlides: 60,
+      noSwiping: false
+    },
+    788: {
+      slidesPerView: 4,
+      slidesPerColumn: 2,
+      noSwiping: false,
+      spaceBetweenSlides: 30
+    },
+    767: {
+      slidesPerView: 2,
+      slidesPerColumn: 2,
+      noSwiping: false,
+      spaceBetweenSlides: 60
     }
-  
-    function initContextNavCarousel() {
-      $(document).find('.outlet-wrap').each(function () {
-        var swiperSlideLength = $(this).find('.swiper-slide').length;
-        $(this).addClass('swiper-with-' + swiperSlideLength + '-slides');
-        var $carouselSlider = new Swiper($(this).find('.slider-outlet-brands'), {
-          loop: false,
-          autoplay: false,
-          slidesPerView: $(this).find('.swiper-slide').length,
-          simulateTouch: true,
-          breakpoints: {
-            767: {
-              slidesPerView: 'auto',
-              centeredSlides: true,
-              spaceBetweenSlides: 0
-            },
-            9999: {
-              centeredSlides: false,
-              slidesPerView: $(this).find('.swiper-slide').length
-            }
-          }
-        });
-  
-        setSpacebetweenContextNavCarousel();
-      });
-    }
-  
-    // register the event handlers
-    $(document).ready(function () {
-      initContextNavCarousel();
-      if ($('.outlet-wrap').length) {
-        $(window).on('resize', function () {
-  
-          setTimeout(function () {
-            setSpacebetweenContextNavCarousel();
-          }, 500);
-  
-        });
-      }
-  
-    });
-  });
-  
+  }
+});
