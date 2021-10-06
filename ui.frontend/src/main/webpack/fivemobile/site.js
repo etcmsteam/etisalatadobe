f4b8e10f97b4a3dabfdf49c20d7c6cea56692920
Ranjitha Padi <42967+cap83167@users.noreply.git.corp.adobe.com>
2021-10-06 20:07:18 +0530

    feature/RefactorUI

--- ui.frontend/src/main/webpack/fivemobile/site.js
+++ ui.frontend/src/main/webpack/fivemobile/site.js
@@ -1,22 +1,22 @@
-$( () => {
-	//On Scroll Functionality
-  $(window).scroll( () => {
-  var windowTop = $(window).scrollTop();
-  if(windowTop > 100) {
-    console.log(windowTop , 'windowTop');
-   $("#main-header").css("border-bottom", "1px solid #ddd").css("height", "60px").css("padding-top", "5px").css("padding-bottom", "5px");
-    //$("#main-header").css("border-bottom", "1px solid #ddd").animate({"padding-top": "5px","height": "60px"},"fast");
-   $('header.experiencefragment .cmp-image').css("margin-left", "12px").css("display", "block").css("width", "36px").css("margin-top", "0");
-   $('.menu').css("margin-top",  "14px");
-  }else{
-    console.log(windowTop , 'windowbottom');
-   $("#main-header").css("border-bottom", "none").css("height", "160px");
-   //$("#main-header").css("border-bottom", "none").animate({"padding-top": "15px", "height": "140px", "padding-bottom": "10px", "padding-left": "7px"},"fast");
-   $('nav ul').css("overflow", "visible");
-   $('header.experiencefragment .cmp-image').css("margin-left", "12px").css("display", "block").css("margin-top", "20px").css("width", "86px"); 
-   $('.menu').css("margin-top",  "60px");
-  }
-});
+$(document).ready(function () {
+  $(window).scroll(() => {
+    var windowTop = $(window).scrollTop();
+    if (windowTop > 100) {
+      $("#main-header").addClass("Scroll-Up");
+      $("#main-header").removeClass("Scroll-Down");
+    } else {
+      $("#main-header").addClass("Scroll-Down");
+      $("#main-header").removeClass("Scroll-Up");
+      $("nav ul").css("overflow", "visible");
+    }
+  });
 
-});
+  $(".menu__icon").on("click", function (e) {
+    e.preventDefault();
+    $(".menu").toggleClass("ShowMenu");
+  });
 
+  $(".menu li a").on("click", function () {
+    $(".menu").removeClass("ShowMenu");
+  });
+});
