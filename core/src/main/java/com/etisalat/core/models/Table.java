d948809bac5c74844c9576cde467a3340e3ad799
Shubham Garg <43896+shubhgarg@users.noreply.git.corp.adobe.com>
2021-10-06 17:42:07 +0530

    Code updates

--- core/src/main/java/com/etisalat/core/models/Table.java
+++ core/src/main/java/com/etisalat/core/models/Table.java
@@ -3,6 +3,8 @@ package com.etisalat.core.models;
 
 import com.day.cq.dam.api.Asset;
 import com.day.cq.dam.api.Rendition;
+import com.drew.lang.StringUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -14,7 +16,10 @@ import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 @Model(adaptables = {Resource.class,
         SlingHttpServletRequest.class})
@@ -29,6 +34,9 @@ public class Table {
 
     @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
     private String csvPath;
+
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+    private String filterCsvPath;
 
     String COMMA_DELIMITER = ",";
 
@@ -92,6 +100,43 @@ public class Table {
 
         }
         return rows;
+    }
+
+    public LinkedHashMap<String, String> getCategories() {
+        return getFilters(0);
+    }
+    public LinkedHashMap<String, String> getLanguages() {
+        return getFilters(1);
+    }
+    public LinkedHashMap<String, String> getPackages() {
+        return getFilters(2);
+    }
+
+    private LinkedHashMap<String, String> getFilters(int index){
+        LinkedHashMap<String, String> row = new LinkedHashMap<>();
+        try {
+            Resource resource = resourceResolver.getResource(filterCsvPath);
+            Asset asset = resource.adaptTo(Asset.class);
+            Rendition rendition = asset.getOriginal();
+            InputStream inputStream = rendition.adaptTo(InputStream.class);
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            int j = 1;
+            while ((line = br.readLine()) != null) {
+                String[] cols = line.split(COMMA_DELIMITER);
+                String item = cols[index];
+                if (item.contains("=")) {
+                    String[] string = item.split("=");
+                    row.put(string[0], string[1]);
+                }
+                else {
+                    row.put(item, StringUtils.EMPTY);
+                }
+            }
+        } catch (Exception e) {
+
+        }
+        return row;
     }
 
 }
