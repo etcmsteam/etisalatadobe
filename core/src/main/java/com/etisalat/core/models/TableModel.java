0aba5fbe815a5e4c5274489114de3ac0e6d26499
Shubham Garg <43896+shubhgarg@users.noreply.git.corp.adobe.com>
2021-10-14 18:21:39 +0530

    Updates

--- core/src/main/java/com/etisalat/core/models/TableModel.java
+++ core/src/main/java/com/etisalat/core/models/TableModel.java
@@ -3,7 +3,6 @@ package com.etisalat.core.models;
 
 import com.day.cq.dam.api.Asset;
 import com.day.cq.dam.api.Rendition;
-import com.drew.lang.StringUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
@@ -12,19 +11,28 @@ import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 @Model(adaptables = {Resource.class,
         SlingHttpServletRequest.class})
 
-public class Table {
+public class TableModel {
+
+    public static final String RTE = "rte";
+    public static final String CSV = "csv";
+    public static final String ALL = "_all";
+    public static final String TRUE = "true";
+    public static final String EQUAL_DELIMITER = "=";
+    public static final String COMMA_DELIMITER = ",";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TableModel.class);
 
     @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
     private String tableSource;
@@ -41,15 +49,14 @@ public class Table {
     @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
     private String filterCsvPath;
 
-    String COMMA_DELIMITER = ",";
 
     @SlingObject
     ResourceResolver resourceResolver;
 
     public String getSorting() {
-        if (tableSource.equals("rte") && doSorting.equals("true")) {
-            return "_all";
-        } else if (tableSource.equals("csv")) {
+        if (tableSource.equals(RTE) && doSorting.equals(TRUE)) {
+            return ALL;
+        } else if (tableSource.equals(CSV)) {
             return "[" + columnToSort + "]";
         }
         return "[]";
@@ -58,20 +65,30 @@ public class Table {
     public List<String> getHeaderList() {
         List<String> headerList = new ArrayList<>();
         try {
-            Resource resource = resourceResolver.getResource(csvPath);
-            Asset asset = resource.adaptTo(Asset.class);
+            final Optional<Resource> resource = Optional.ofNullable(resourceResolver.getResource(csvPath));
+            if (!resource.isPresent()) { // if the resource doesn't exists
+                LOGGER.error("While reading the header the resource doesn't exists at the path {}",
+                        csvPath);
+                return headerList;
+            }
+
+            Asset asset = resource.get().adaptTo(Asset.class);
             Rendition rendition = asset.getOriginal();
             InputStream inputStream = rendition.adaptTo(InputStream.class);
             BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
             String line;
             while ((line = br.readLine()) != null) {
-                String[] values = line.split(COMMA_DELIMITER);
-                headerList = Arrays.asList(values);
+                if (line.contains(COMMA_DELIMITER)) {
+                    String[] values = line.split(COMMA_DELIMITER);
+                    headerList = Arrays.asList(values);
+                }
+
                 break;
             }
 
-        } catch (Exception e) {
-
+        } catch (IOException e) {
+            LOGGER.error("The exception occurred in getting header while reading table csv {}",
+                    csvPath);
         }
 
         return headerList;
@@ -81,32 +98,44 @@ public class Table {
 
         List<LinkedHashMap<String, String>> rows = new ArrayList<>();
         try {
-            Resource resource = resourceResolver.getResource(csvPath);
-            Asset asset = resource.adaptTo(Asset.class);
+
+            final Optional<Resource> resource = Optional.ofNullable(resourceResolver.getResource(csvPath));
+
+            if (!resource.isPresent()) { // if the resource doesn't exists
+                LOGGER.error("The resource doesn't exists at the path {}",
+                        csvPath);
+                return rows;
+            }
+
+            Asset asset = resource.get().adaptTo(Asset.class);
             Rendition rendition = asset.getOriginal();
             InputStream inputStream = rendition.adaptTo(InputStream.class);
             BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
             String line;
             int j = 1;
             while ((line = br.readLine()) != null) {
-                String[] values = line.split(COMMA_DELIMITER);
-                if (j > 1) {
-                    LinkedHashMap<String, String> row = new LinkedHashMap<>();
-                    for (int i = 0; i < values.length; i = i + 2) {
-                        String key = values[i];
-                        String value = StringUtils.EMPTY;
-                        if(values.length > i+1){
-                            value = values[i + 1];
-                        }
+                if (line.contains(COMMA_DELIMITER)) {
+                    String[] values = line.split(COMMA_DELIMITER);
+                    if (j > 1) {
+                        LinkedHashMap<String, String> row = new LinkedHashMap<>();
+                        for (int i = 0; i < values.length; i = i + 2) {
+                            String key = values[i];
+                            String value = StringUtils.EMPTY;
+                            if (values.length > i + 1) {
+                                value = values[i + 1];
+                            }
 
-                        row.put(key,value);
+                            row.put(key, value);
+                        }
+                        rows.add(row);
                     }
-                    rows.add(row);
+
                 }
                 j++;
             }
-        } catch (Exception e) {
-
+        } catch (IOException e) {
+            LOGGER.error("The exception occurred while reading table csv {}",
+                    csvPath);
         }
         return rows;
     }
@@ -114,36 +143,45 @@ public class Table {
     public LinkedHashMap<String, String> getCategories() {
         return getFilters(0);
     }
+
     public LinkedHashMap<String, String> getLanguages() {
         return getFilters(1);
     }
+
     public LinkedHashMap<String, String> getPackages() {
         return getFilters(2);
     }
 
-    private LinkedHashMap<String, String> getFilters(int index){
+    private LinkedHashMap<String, String> getFilters(int index) {
         LinkedHashMap<String, String> row = new LinkedHashMap<>();
         try {
-            Resource resource = resourceResolver.getResource(filterCsvPath);
-            Asset asset = resource.adaptTo(Asset.class);
+            final Optional<Resource> resource = Optional.ofNullable(resourceResolver.getResource(filterCsvPath));
+            if (!resource.isPresent()) { // if the resource doesn't exists
+
+                LOGGER.error("The resource doesn't exists at the path {}",
+                        filterCsvPath);
+                return row;
+            }
+
+            Asset asset = resource.get().adaptTo(Asset.class);
             Rendition rendition = asset.getOriginal();
             InputStream inputStream = rendition.adaptTo(InputStream.class);
             BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
             String line;
-            int j = 1;
+
             while ((line = br.readLine()) != null) {
                 String[] cols = line.split(COMMA_DELIMITER);
                 String item = cols[index];
-                if (item.contains("=")) {
-                    String[] string = item.split("=");
+                if (item.contains(EQUAL_DELIMITER)) {
+                    String[] string = item.split(EQUAL_DELIMITER);
                     row.put(string[0], string[1]);
-                }
-                else {
+                } else {
                     row.put(item, StringUtils.EMPTY);
                 }
             }
-        } catch (Exception e) {
-
+        } catch (IOException e) {
+            LOGGER.error("The exception occurred while reading filter csv {}",
+                    filterCsvPath);
         }
         return row;
     }
