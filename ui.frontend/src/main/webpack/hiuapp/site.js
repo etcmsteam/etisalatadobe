88e6d70b9ed4264045d6c10ddbcdd75eb73bdd39
Rahil <cap66173@adobe.com>
2021-10-28 01:36:25 +0530

    Bugfixes - 4

--- ui.frontend/src/main/webpack/hiuapp/site.js
+++ ui.frontend/src/main/webpack/hiuapp/site.js
@@ -17,21 +17,52 @@ $(document).ready(function () {
     $(".main-nav__list").addClass("ShowMenu");
     $(".main-nav__list").toggleClass("Scroll-Down");
     $(".main-nav__list.ShowMenu").slideToggle("slow");
-
   });
 
   $(".menu li a").on("click", function () {
     $(".ShowMenu").slideToggle("slow");
     $(".main-nav__list").removeClass("ShowMenu");
-
-
   });
 });
 
-$(document).on('click', '.main-nav__item a[href^="#"]','.scrollUp', function (event) {
-  event.preventDefault();
-
-  $('html, body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top - 40
-  }, 300);
-});
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          }
+        );
+      }
+    }
+  });
