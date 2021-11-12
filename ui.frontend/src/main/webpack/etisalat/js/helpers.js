a6a8b815606b3740a97ff412075485a04ea65b62
Deepak Sharma <deepaksharma@adobe.com>
2021-11-12 22:08:34 +0530

    Implement slider pending variation and resolve existing blocker issues for - ETSL-173

---
+++ ui.frontend/src/main/webpack/etisalat/js/helpers.js
@@ -0,0 +1,7 @@
+
+export const IsMediaQuery = {
+	xs: window.matchMedia('(min-width: 576px)'),
+	sm: window.matchMedia('(min-width: 768px)'),
+	md: window.matchMedia('(min-width: 992px)'),
+	lg: window.matchMedia('(min-width: 1200px)'),
+};
