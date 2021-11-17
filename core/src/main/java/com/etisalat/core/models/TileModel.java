d3c2b2ce29c0f88c2a41f7d6f625d867ec06c742
Gopinath <gopinath.chakala@capgemini.com>
2021-11-17 23:17:09 +0530

    update grid layout property

--- core/src/main/java/com/etisalat/core/models/TileModel.java
+++ core/src/main/java/com/etisalat/core/models/TileModel.java
@@ -53,6 +53,10 @@ public class TileModel {
 	@SlingObject
 	private ResourceResolver resourceResolver;
 	
+	private static final String TILE_CONTAINER_RESOURCETYPE = "etisalat/components/tilecontainer";
+	
+	private static final String SWIPER_CONTAINER_RESOURCETYPE = "etisalat/components/swipercontainer";
+	
 	/**
 	 * The current request.
 	 */
@@ -175,7 +179,8 @@ public class TileModel {
 	public String getTileBoxContainerLayout() {		
 		Resource tileContainerResource = currentResource.getParent();
 		if (null != tileContainerResource
-				&& tileContainerResource.getResourceType().equals("etisalat/components/tilecontainer")) {
+				&& (tileContainerResource.getResourceType().equals(TILE_CONTAINER_RESOURCETYPE) ||
+						tileContainerResource.getResourceType().equals(SWIPER_CONTAINER_RESOURCETYPE))) {
               return tileContainerResource.getValueMap().get("tileBoxLayout",StringUtils.EMPTY);
 		}
 		return StringUtils.EMPTY;
