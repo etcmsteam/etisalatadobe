9dcc029846a0afd48b047d73478bc12aee8dd25b
Gopinath <gopinath.chakala@capgemini.com>
2021-11-17 22:36:32 +0530

    added mobile view for feature offer content

--- ui.frontend/src/main/webpack/ewallet/offers-overview-slider.js
+++ ui.frontend/src/main/webpack/ewallet/offers-overview-slider.js
@@ -1,18 +1,39 @@
-var swiper = new Swiper(".swiper-container-horizontal", {
-  slidesPerView: 3,
-  spaceBetween: 25,
-  noSwiping: true,
-  noSwipingClass: "no-swap-desktop-new",
-  breakpoints: {
-    767: {
-      slidesPerView: 3,
-      spaceBetween: 20,
-      noSwiping: false,
-    },
-    736: {
-      slidesPerView: "auto",
-      spaceBetween: 20,
-      noSwiping: false,
-    },
-  },
+$(document).ready(function() {
+    var swiper = new Swiper(".slider-offerNew", {
+        slidesPerView: 3,
+        spaceBetween: 25,
+        noSwiping: true,
+        noSwipingClass: "no-swap-desktop-new",
+        breakpoints: {
+            767: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+                noSwiping: false,
+            },
+            736: {
+                slidesPerView: "auto",
+                spaceBetween: 20,
+                noSwiping: false,
+            },
+        },
+    });
+
+    var swiper = new Swiper('.slider-featureOffer', {
+        slidesPerView: 3,
+        spaceBetween: 25,
+        noSwiping: true,
+        noSwipingClass: 'no-swap-desktop-feature',
+        breakpoints: {
+            767: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+                noSwiping: false
+            },
+            736: {
+                slidesPerView: 'auto',
+                spaceBetween: 20,
+                noSwiping: false
+            }
+        }
+    });
 });
