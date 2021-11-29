f3d4d3615638aa2369a5724575ee0a031e699e51
Malvankar <ashwini.malvankar@capgemini.com>
2021-11-30 01:22:48 +0530

    reverted folder structure code

--- ui.frontend/src/main/webpack/etisalat/main.js
+++ ui.frontend/src/main/webpack/etisalat/main.js
@@ -1,18 +1,12 @@
 // Javascript or Typescript
-import { Initializer } from "./js/initializer";
+import { Initializer } from "./initializer.js";
 
 import "./**/*.js";
 import "./js/forms";
-import "./js/accordion/*.js";
-import "./js/clientcode/*.js";
-import "./js/language/*.js";
-import "./js/site/*.js";
-
 // Stylesheets
 import "./main.scss";
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  // eslint-disable-next-line no-new
   new Initializer();
 });
