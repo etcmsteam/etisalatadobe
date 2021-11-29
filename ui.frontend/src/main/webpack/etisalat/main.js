4f31595a3494897981d61a4b98bc3d6fdc45156c
Deepak Sharma <deepaksharma@adobe.com>
2021-11-29 11:50:29 +0530

    fixing folder path issues for scss files

--- ui.frontend/src/main/webpack/etisalat/main.js
+++ ui.frontend/src/main/webpack/etisalat/main.js
@@ -1,17 +1,18 @@
 // Javascript or Typescript
-import { Initializer } from "./initializer.js";
+import { Initializer } from "./js/initializer";
 
-import "./**/*.js";
-import "./js/forms";
-import "./js/accordion/*.js";
-import "./js/clientcode/*.js";
-import "./js/language/*.js";
-import "./js/site/*.js";
+// import "./**/*.js";
+// import "./js/forms";
+// import "./js/accordion/*.js";
+// import "./js/clientcode/*.js";
+// import "./js/language/*.js";
+// import "./js/site/*.js";
 
 // Stylesheets
 import "./main.scss";
 
 
 document.addEventListener("DOMContentLoaded", () => {
+  // eslint-disable-next-line no-new
   new Initializer();
 });
