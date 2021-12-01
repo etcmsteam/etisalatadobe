b62f5d4a21e7984accad4a016f10fb32543da2d4
Ashwini <ashwini.malvankar@capgemini.com>
2021-12-01 23:12:03 +0530

    css changes

--- ui.frontend/src/main/webpack/etisalat/main.js
+++ ui.frontend/src/main/webpack/etisalat/main.js
@@ -1,8 +1,14 @@
 // Javascript or Typescript
 import { Initializer } from "./initializer.js";
 
+
 import "./**/*.js";
 import "./js/forms";
+import "./js/accordion/*.js";
+import "./js/clientcode/*.js";
+import "./js/language/*.js";
+import "./js/site/*.js";
+
 // Stylesheets
 import "./main.scss";
 
