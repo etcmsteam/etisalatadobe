ca87452dc2dcf107c08af51eefa665c7440be3e4
Rahil <cap66173@adobe.com>
2021-12-07 13:46:19 +0530

    eWallet Refactor - 8.2

--- ui.frontend/src/main/webpack/ewallet/mobile-dropdown.js
+++ ui.frontend/src/main/webpack/ewallet/mobile-dropdown.js
@@ -1,12 +1,9 @@
 $(".menu__icon").on("click", function (e) {
   e.preventDefault();
   $(".menu__icon").toggleClass("CloseBtn");
-  $(".main-nav__list").addClass("ShowMenu");
-  $(".main-nav__list.ShowMenu").slideToggle("slow");
+  $(".MenuWrapper").slideToggle("slow");
 });
 
 $(".menu li a").on("click", function () {
-  $(".ShowMenu").slideToggle("slow");
-  $(".main-nav__list").removeClass("ShowMenu");
+  $(".MenuWrapper").slideToggle("slow");
 });
-
