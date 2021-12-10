0ccdb39e930d5ca3c8d514177bb8b9db7603b3d3
Gopinath <gopinath.chakala@capgemini.com>
2021-12-10 13:00:21 +0530

    added search component

---
+++ core/src/main/java/com/etisalat/core/models/SiteSearch.java
@@ -0,0 +1,10 @@
+package com.etisalat.core.models;
+
+import java.util.List;
+
+public interface SiteSearch {
+
+	List<LinkModel> getQuickLinksItems();
+	
+	List<LinkModel> getBrandItems();
+}
---
+++ core/src/main/java/com/etisalat/core/models/impl/SiteSearchImpl.java
@@ -0,0 +1,52 @@
+package com.etisalat.core.models.impl;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+
+import com.etisalat.core.models.LinkModel;
+import com.etisalat.core.models.SiteSearch;
+
+@Model(adaptables = { Resource.class, SlingHttpServletRequest.class }, adapters = { SiteSearch.class }, resourceType = {
+		SiteSearchImpl.RESOURCE_TYPE })
+public class SiteSearchImpl implements SiteSearch {
+
+	public static final String RESOURCE_TYPE = "etisalat/components/search";
+
+	private static final String QUICKLINK_ITEMS = "quicklinks";
+
+	private static final String BRAND_ITEMS = "brandItems";
+
+	@SlingObject
+	@Optional
+	private Resource currentRes;
+
+	private List<LinkModel> getItems(String itemRes) {
+		List<LinkModel> subItemList = new ArrayList<>();
+		Resource subItemRes = currentRes.getChild(itemRes);
+		if (null != subItemRes) {
+			subItemRes.listChildren().forEachRemaining(resource -> {
+				LinkModel linkModel = resource.adaptTo(LinkModel.class);
+				subItemList.add(linkModel);
+			});
+		}
+		return subItemList;
+	}
+
+	@Override
+	public List<LinkModel> getQuickLinksItems() {
+		return Collections.unmodifiableList(getItems(QUICKLINK_ITEMS));
+	}
+
+	@Override
+	public List<LinkModel> getBrandItems() {
+		return Collections.unmodifiableList(getItems(BRAND_ITEMS));
+	}
+
+}
