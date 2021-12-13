63d70a1e47a375e6319d2116193d006d7aeb3aad
Deepak Sharma <deepaksharma@adobe.com>
2021-12-13 21:24:13 +0530

    CSS JS changes to fix ETSL-533

--- ui.frontend/src/main/webpack/etisalat/js/forms/newsletter-subscription.js
+++ ui.frontend/src/main/webpack/etisalat/js/forms/newsletter-subscription.js
@@ -1,19 +1,21 @@
-import { FORM_VALIDATION_MESSAGES } from '../constant';
+/* eslint-disable */
+import { FORM_VALIDATION_MESSAGES } from "../constant";
 
 (function () {
-  const NO_SCROLL_CLASS = 'no-scroll';
-  const $SUCCSS_POP_UP = $('.cmp-experiencefragment--Newsletter-subscription-pop-up');
-  const $BODY = $('body');
-  const $OVERLAY_CLOSE_CTA = $('.form-close');
-  const $FORM = $('#newsletterSubscription');
-  const $SUBMIT_CTA = $('#newsletterSubscription .cmp-form-button');
+  const NO_SCROLL_CLASS = "no-scroll";
+  const $SUCCSS_POP_UP = $(".cmp-experiencefragment--Newsletter-subscription-pop-up");
+  const $BODY = $("body");
+  const $OVERLAY_CLOSE_CTA = $(".form-close");
+  const $FORM = $("#newsletterSubscription");
+  const $SUBMIT_CTA = $("#newsletterSubscription .cmp-form-button");
+  const $DYNAMIC_EMAIL = $("#form-popup-box .dynamicEmail");
 
   if (!$FORM.length) {
     return false;
   }
 
-  $SUBMIT_CTA.on('click', function () {
-    if ($FORM.valid() == false) {
+  $SUBMIT_CTA.on("click", function () {
+    if ($FORM.valid() === false) {
       return false;
     }
   });
@@ -21,14 +23,14 @@ import { FORM_VALIDATION_MESSAGES } from '../constant';
   let messagelocal;
   const validateMessage = {
     en: {
-      emailAddress: FORM_VALIDATION_MESSAGES.EN.EmailAddress,
+      EmailAddress: FORM_VALIDATION_MESSAGES.EN.EmailAddress,
     },
     ar: {
-      emailAddress: FORM_VALIDATION_MESSAGES.AR.EmailAddress,
+      EmailAddress: FORM_VALIDATION_MESSAGES.AR.EmailAddress,
     },
   };
 
-  if (document.documentElement.lang == 'ar') {
+  if (document.documentElement.lang === "ar") {
     messagelocal = validateMessage.ar;
   } else {
     messagelocal = validateMessage.en;
@@ -41,21 +43,23 @@ import { FORM_VALIDATION_MESSAGES } from '../constant';
         realalphabeticlatinarabic: true,
         maxlength: 25,
       },
-      EmailAddress: {
+      email: {
         required: true,
         email: true,
       },
     },
     messages: messagelocal,
-    submitHandler: function (form) {
-      //const formData = $FORM.serialize();
+    submitHandler: function () {
+      // const formData = $FORM.serialize();
+      const GET_EMAIL = $("input[name='EmailAddress']").val();
+      $DYNAMIC_EMAIL.text(GET_EMAIL);
       $SUCCSS_POP_UP.show();
       $BODY.addClass(NO_SCROLL_CLASS);
       return false;
     },
   });
 
-  $OVERLAY_CLOSE_CTA.on('click', (event) => {
+  $OVERLAY_CLOSE_CTA.on("click", (event) => {
     $SUCCSS_POP_UP.hide();
     $BODY.removeClass(NO_SCROLL_CLASS);
   });
