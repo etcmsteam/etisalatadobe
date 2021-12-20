099c6d67096bb9e629f657c269b1a19933eb5007
Gopinath <gopinath.chakala@capgemini.com>
2021-12-20 15:58:50 +0530

    updated send notifications sling resource type annotation

--- core/src/main/java/com/etisalat/core/servlets/SendNotificationServlet.java
+++ core/src/main/java/com/etisalat/core/servlets/SendNotificationServlet.java
@@ -6,19 +6,24 @@ import java.io.InputStreamReader;
 import java.net.SocketTimeoutException;
 
 import javax.servlet.Servlet;
+import org.apache.sling.api.servlets.HttpConstants;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.propertytypes.ServiceDescription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.etisalat.core.services.SendNotificationService;
 
-@Component(immediate=true, service={Servlet.class}, property={"sling.servlet.methods=post", "sling.servlet.paths=/bin/api/sendNotification"})
+@Component(service = { Servlet.class })
+@SlingServletResourceTypes(resourceTypes = "etisalat/components/page", methods = HttpConstants.METHOD_POST, selectors = "sendnotification", extensions = "html")
+@ServiceDescription("Etisalat Send Notification Servlet")
 public class SendNotificationServlet extends SlingAllMethodsServlet {
 
 	private static final Logger LOG = LoggerFactory.getLogger(SendNotificationServlet.class);
@@ -28,39 +33,32 @@ public class SendNotificationServlet extends SlingAllMethodsServlet {
 	private static final String JSON_STRING = "";
 
 	@Reference
-	private transient SendNotificationService sendNotificationService; 
+	private transient SendNotificationService sendNotificationService;
 
-	protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response)
-	{ 
-		try
-		{					   
+	@Override
+	protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) {
+		try {
 			String json = JSON_STRING;
 			int status = BAD_REQUEST;
-			BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));				
-			if(br != null){
+			if (null != request.getInputStream()) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
 				json = br.readLine();
 			}
-			if(!StringUtils.isEmpty(json)) {
+			if (!StringUtils.isEmpty(json)) {
 				status = sendNotificationService.postFormData(json);
 			}
 
-			if(status == RESPONSE_OK) {
+			if (status == RESPONSE_OK) {
 				response.setStatus(status);
-			}
-			else {
+			} else {
 				response.setStatus(status);
-				LOG.error("Send Notification Service Failed and API response is"+ status);
+				LOG.error("Send Notification Service Failed and API response is {}", status);
 			}
-
 		}
-		
-		catch(SocketTimeoutException e)
-		{
-			LOG.error("Send Notification Service Time Out " + e.getMessage());
-		}
-		catch(IOException e)
-		{
-			LOG.error("Send Notification Service Input Stream Parsing Error",e.getMessage());
+		catch (SocketTimeoutException e) {
+			LOG.error("Send Notification Service Time Out {}", e.getMessage());
+		} catch (IOException e) {
+			LOG.error("Send Notification Service Input Stream Parsing Error {}", e.getMessage());
 		}
 	}
 
