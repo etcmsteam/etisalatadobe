038548915ff58a5a21ea5d57494ef104b65ecbec
Krishnarjuna Rao Routhu <cap77715@adobe.com>
2022-01-11 20:04:44 +0530

    added code changes for  form refacting issues

---
+++ ui.frontend/src/main/webpack/ewallet/js/forms/tesla-forms.js
@@ -0,0 +1,81 @@
+import { FORM_VALIDATION_MESSAGES } from "./constant";
+(function () {
+  const $FORM = $("#new_form");
+  const $SUBMIT_CTA = $("#new_form .cmp-form-button");
+
+  if (!$FORM.length) {
+    return false;
+  }
+
+  $SUBMIT_CTA.on("click", function () {
+    if ($FORM.valid() == false) {
+      return false;
+    }
+  });
+
+  let messagelocal;
+  const validateMessage = {
+    en: {
+      firstName: FORM_VALIDATION_MESSAGES.EN.firstName,
+      lastName: FORM_VALIDATION_MESSAGES.EN.lastName,
+      companyName: FORM_VALIDATION_MESSAGES.EN.companyName,
+      emirate: FORM_VALIDATION_MESSAGES.EN.emirate,
+      phoneNumber: FORM_VALIDATION_MESSAGES.EN.phoneNumber,
+      emailAddress: FORM_VALIDATION_MESSAGES.EN.emailAddress,
+    },
+    ar: {
+      number: FORM_VALIDATION_MESSAGES.AR.number,
+      firstName: FORM_VALIDATION_MESSAGES.AR.firstName,
+      lastName: FORM_VALIDATION_MESSAGES.AR.lastName,
+      companyName: FORM_VALIDATION_MESSAGES.AR.companyName,
+      emirate: FORM_VALIDATION_MESSAGES.EN.emirate,
+      phoneNumber: FORM_VALIDATION_MESSAGES.AR.phoneNumber,
+      emailAddress: FORM_VALIDATION_MESSAGES.AR.emailAddress,
+    },
+  };
+
+  if (document.documentElement.lang == "ar") {
+    messagelocal = validateMessage.ar;
+  } else {
+    messagelocal = validateMessage.en;
+  }
+
+  $FORM.validate({
+    rules: {
+      firstName: {
+        required: true,
+        realalphabeticlatinarabic: true,
+        maxlength: 248,
+      },
+      lastName: {
+        required: true,
+        realalphabeticlatinarabic: true,
+        maxlength: 248,
+      },
+      companyName: {
+        required: true,
+        realalphabeticlatinarabic: true,
+        maxlength: 248,
+      },
+      emirate: {
+        required: true,
+      },
+      phoneNumber: {
+        required: true,
+        number: true,
+        digits: true,
+        pattern: /^(050|054|055|056|052)/,
+        minlength: 10,
+        maxlength: 10,
+      },
+      emailAddress: {
+        required: true,
+        email: true,
+      },
+    },
+    messages: messagelocal,
+    submitHandler: function (form) {
+      return false;
+    },
+  });
+})();
---
+++ ui.frontend/src/main/webpack/ewallet/js/global/index.js
@@ -0,0 +1,7 @@
+import "./common";
+import "./autocomplete";
+import "./chatbot";
+import "./ewallet-mobile-slider";
+import "./mobile-dropdown";
+import "./offers-overview-slider";
+import "./site";
