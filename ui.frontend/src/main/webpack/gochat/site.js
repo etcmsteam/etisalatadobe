e53f53b604f28df8dcb46c9dec0dc5b5e5bc666e
Paresh Salvi <sok32265@adobe.com>
2022-01-21 17:07:56 +0530

    wip gochat header and footer

--- ui.frontend/src/main/webpack/gochat/site.js
+++ ui.frontend/src/main/webpack/gochat/site.js
@@ -1,4 +1,16 @@
-//dynamic year
-$(document).ready(function () {
-$('.footer-copyright-year').html(new Date().getFullYear());
+
+$(window).scroll(function(){
+    let sticky = $('.cmp-experiencefragment--header');
+    let scroll = $(window).scrollTop();
+
+    if (scroll >= 100) {
+        sticky.addClass('cmp-experiencefragment--sticky');
+        $('.logo--sticky').attr('style', 'display: block !important');
+        $('.logo').hide();
+    }
+    else {
+        sticky.removeClass('cmp-experiencefragment--sticky');
+        $('.logo--sticky').attr('style', 'display: none !important');
+        $('.logo').show();
+    }
 });
