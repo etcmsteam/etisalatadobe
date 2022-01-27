c877c18200e2929dfbde0256480cee59ad68e2a5
Gopinath <gopinath.chakala@capgemini.com>
2022-01-27 17:18:49 +0530

    build fixes

--- ui.frontend/src/main/webpack/careers/main.js
+++ ui.frontend/src/main/webpack/careers/main.js
@@ -1,6 +1,15 @@
+// Javascript or Typescript
+import { Initializer } from "./initializer.js";
+
+
+import "./js/core-components/*.js";
+import "./js/custom-components/*.js";
+
 // Stylesheets
 import "./main.scss";
 
-// Javascript
-import "./**/*.js";
-import "./site.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  // eslint-disable-next-line no-new
+  new Initializer();
+});
