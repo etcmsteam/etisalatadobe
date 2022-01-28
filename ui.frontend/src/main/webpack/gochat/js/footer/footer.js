483afc0c95987dbf5ba866cf060c3a5eae9700ac
Paresh Salvi <sok32265@adobe.com>
2022-01-29 00:35:38 +0530

    smooth scroll to section updated

--- ui.frontend/src/main/webpack/gochat/js/footer/footer.js
+++ ui.frontend/src/main/webpack/gochat/js/footer/footer.js
@@ -1,5 +1,4 @@
 // dynamic year for copyright text
-
 document.addEventListener("DOMContentLoaded", function () {
   let copyrightElemsCont;
   if (document.querySelector(".cmp-experiencefragment--footer-2")) {
@@ -12,3 +11,36 @@ document.addEventListener("DOMContentLoaded", function () {
   const copyrightElems = copyrightElemsCont.querySelector(".cmp-text p");
   copyrightElems.innerHTML = "&copy; " + new Date().getFullYear() + " Gochat";
 });
+
+
+const footerLinks = document.querySelectorAll('footer ul li a');
+footerLinks.forEach(footerLink => {
+  footerLink.addEventListener('click', function (e){
+    e.preventDefault();
+    let href = footerLink.getAttribute("href");
+    if (href.indexOf("/") === -1) {
+        document.querySelector(footerLink.getAttribute("href")).scrollIntoView({
+          behavior: "smooth",
+          block: "center",
+        });
+    } else {
+      window.location = href;
+    }
+  });
+});
+
+// .click(function (e) {
+//   e.preventDefault();
+//   let href = $(this).attr("href");
+//   if (href.indexOf("/") === -1) {
+//     let offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+//     $("html, body").stop().animate(
+//       {
+//         scrollTop: offsetTop,
+//       },
+//       300,
+//     );
+//   } else {
+//     window.location = href;
+//   }
+// });
