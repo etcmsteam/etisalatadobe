6c78246122279b6986a21c75ea6e210d5f0214b2
cap36461 <cap36461@adobe.com>
2022-02-01 13:37:51 +0530

    ETSL-475-Tabs js update for scroll smoothing behaviour issue

--- ui.frontend/src/main/webpack/etisalat/js/core-components/tabs.js
+++ ui.frontend/src/main/webpack/etisalat/js/core-components/tabs.js
@@ -1,19 +1,8 @@
 /* eslint-disable no-undef */
 (function ($) {
-  function initTabs() {
-    const tabContainer = $(".cmp-tabs__tab");
-    
-    tabContainer.each(function (index) {
-      tabContainer[index].addEventListener("click", function () {
-        const tabsActiveId = $(this).attr("id") + 'panel';
-        const offset = $("#" + tabsActiveId).offset();
-        window.scrollTo(offset.left, offset.top);
-      });
-    });
-  }
-
   $(document).ready(function () {
-      initTabs();
+    $(".cmp-tabs__tab").click(function () {
+      window.scrollTo(0, 0);
+    });
   });
-
 })(jQuery);
