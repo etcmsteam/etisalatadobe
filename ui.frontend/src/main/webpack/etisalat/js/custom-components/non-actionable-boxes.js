5bacffb2134718cba24657519b88657e2509078d
Yuvarj Srinivasan <diz32873@adobe.com>
2022-02-09 22:45:16 +0530

    ETSL-995 Non actionable boxes changes
    ETSL-995 Non actionable boxes changes

--- ui.frontend/src/main/webpack/etisalat/js/custom-components/non-actionable-boxes.js
+++ ui.frontend/src/main/webpack/etisalat/js/custom-components/non-actionable-boxes.js
@@ -2,20 +2,23 @@
   // register the event handlers
   $(document).ready(function () {
     // open youtube video and close by removing src and then add
-    $(".youtube-video-link").on("click", function (e) {
-      e.preventDefault();
-      var modalCTA = $(this).next(".mediaCtaVideo");
-      modalCTA.modal().show();
-      if($('body').hasClass('modal-open')) {
-        $("body").parent().css('overflow', 'hidden');
-      }
-    });
+    var nonActionBox = $(ducument).find(".non-action-boxes-two-cols-4-0");
+    if (nonActionBox.length > 0) {
+      $(".youtube-video-link").on("click", function (e) {
+        e.preventDefault();
+        var modalCTA = $(this).next(".mediaCtaVideo");
+        modalCTA.modal().show();
+        if ($("body").hasClass("modal-open")) {
+          $("body").parent().css("overflow", "hidden");
+        }
+      });
 
-    $(".youtube-popup-container").on("hidden.bs.modal", function () {
-      var src = $(this).find("iframe").attr("src");
-      $(this).find("iframe").attr("src", "");
-      $(this).find("iframe").attr("src", src.replace("autoplay=1", ""));
-      $("html").removeAttr("style");
-    });
+      $(".youtube-popup-container").on("hidden.bs.modal", function () {
+        var src = $(this).find("iframe").attr("src");
+        $(this).find("iframe").attr("src", "");
+        $(this).find("iframe").attr("src", src.replace("autoplay=1", ""));
+        $("html").removeAttr("style");
+      });
+    }
   });
 })();
