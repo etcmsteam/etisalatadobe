0154320468885f82168c958d3afd14f3e9384404
Gopinath <gopinath.chakala@capgemini.com>
2022-02-10 21:11:48 +0530

    added error handler

--- core/src/main/java/com/etisalat/core/models/ErrorHandlerModel.java
+++ core/src/main/java/com/etisalat/core/models/ErrorHandlerModel.java
@@ -1,7 +1,13 @@
 package com.etisalat.core.models;
 
-import javax.inject.Inject;
+import java.util.Optional;
 
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.Resource;
@@ -9,6 +15,7 @@ import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
+import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,24 +30,68 @@ public class ErrorHandlerModel {
   @Default(values = {PageConstants.DEFAULT_STATUS_CODE})
   private String errorCode;
   
+  @Self
+  private SlingHttpServletRequest slingRequest;
+  
   /**
    * The current request.
    */
   @ScriptVariable
   private SlingHttpServletResponse response;
 
+  
+  @PostConstruct
+  protected void init() {
+    String requestURI = this.slingRequest.getRequestPathInfo().getResourcePath();
+    response.setStatus(Integer.parseInt(errorCode));
+    response.setContentType("text/html");
+    response.setCharacterEncoding(PageConstants.UTF_8);
+     if (StringUtils.isNotBlank(requestURI)) {
+      this.errorPage = getErrorPageFromRequestedUrl(this.errorCode, requestURI);
+      LOGGER.debug("Page path - init method {}", this.errorPage);
+    }
+      
+  }
+  
+  private String getErrorPageFromRequestedUrl(String errorCode, String requestURI) {
+    Page resolvedPage = getPageFromPath(requestURI);
+    if (resolvedPage != null)
+      return getErrorPathFromPage(errorCode, resolvedPage); 
+    return null;
+  }
+  
+  private Page getPageFromPath(String requestURI) {
+   final PageManager pageManager = this.slingRequest.getResourceResolver().adaptTo(PageManager.class);
+    while (requestURI.contains("/")) {
+      if (Optional.ofNullable(pageManager).isEmpty()) {
+        LOGGER.debug("Pagemanager is null");
+        return null;
+      } 
+      final Page page = pageManager.getContainingPage(requestURI);
+      if (Optional.ofNullable(page).isEmpty()) {
+        requestURI = requestURI.substring(0, requestURI.lastIndexOf("/"));
+        LOGGER.debug("Request URI is {}", requestURI);
+        continue;
+      } 
+      LOGGER.debug("Page path is {}", page.getPath());
+      return page;
+    } 
+    return null;
+  }
+  
+  private String getErrorPathFromPage(String errorCode, Page resolvedPage) {
+    if (resolvedPage.hasChild("error"))
+      return new StringBuilder().append(resolvedPage.getPath()).append("/error/").append(errorCode).toString(); 
+    if (resolvedPage.getParent() != null && resolvedPage.getDepth() >= 2)
+      return getErrorPathFromPage(errorCode, resolvedPage.getParent()); 
+    return null;
+  }
+
   /**
    * @return the errorPage
    */
   public String getErrorPage() {
-   final String errorPageURL = "/content/etisalat/ae/en/error/" + errorCode;
-   LOGGER.debug("Error Page path - init method {}", this.errorPage);
-	    
-   response.setStatus(Integer.parseInt(errorCode));
-   response.setContentType("text/html");
-   response.setCharacterEncoding(PageConstants.UTF_8);
-   
-   return errorPageURL;
+    return this.errorPage;
   }
 
 
