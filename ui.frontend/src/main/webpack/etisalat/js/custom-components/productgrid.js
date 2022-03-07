5a8dfe9b29eb35519a701f3b75eb322ecb18bd1b
Yuvarj Srinivasan <diz32873@adobe.com>
2022-03-07 17:09:22 +0530

    Product grid with filter js changes
    Product grid with filter js changes

--- ui.frontend/src/main/webpack/etisalat/js/custom-components/productgrid.js
+++ ui.frontend/src/main/webpack/etisalat/js/custom-components/productgrid.js
@@ -97,6 +97,29 @@ import { swiperInit } from "../../../global/js/swiperInitialize";
       });
     }
 
+    var recommendedCardsCarousal;
+    function initSwiperRecommendedCards() {
+      recommendedCardsCarousal = new Swiper(".most-recommended-products .swiper-container", {
+        loop: false,
+        autoplay: false,
+        slidesPerView: 3,
+        spaceBetween: 24,
+        pagination: ".swiper-pagination",
+        paginationClickable: true,
+        breakpoints: {
+          767: {
+            slidesPerView: 1.25,
+          },
+          992: {
+            slidesPerView: 2.5,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+        },
+      });
+    }
+
     function setSpacebetweenTableCarousel() {
       var $carousel = $(".plans-table .swiper-container");
       var spaceBetween = 0;
@@ -209,6 +232,121 @@ import { swiperInit } from "../../../global/js/swiperInitialize";
           setSpacebetweenTableCarousel();
         }, 500);
       });
+
+
+      //Product cards no tabs filter
+      var radios = [];
+      var selected = [];
+
+      $(".category-list-wrap input").each(function () {
+        radios.push($(this));
+      });
+
+      for (var i = 0; i < radios.length; i++) {
+        $(radios[i]).each(function (index) {
+          var self = $(this);
+          if ($(this).prop("checked")) {
+            selected.push($(this));
+
+            // passing data-label and id to function for filtering
+            filterCards($(this).attr("data-label"), $(this));
+          }
+        });
+      }
+
+      // on radio selction show hide right panel ( container )
+      $(".category-list-wrap input").change(function () {
+        if ($(this).is(":checked")) {
+          // check if the radio is checked
+          filterCards($(this).attr("data-label"), $(this));
+        }
+        // update the swiper after filtering the slides
+        if (window.innerWidth < 992) {
+          recommendedCardsCarousal.update();
+          recommendedCardsCarousal.slideReset(100, false);
+        }
+      });
+
+      function filterCards(radioInput, selfFilter) {
+        var filter = $(selfFilter).closest(".category-list-wrap");
+        //var common = $(selfFilter).closest('.common');
+        //var currentTab = $('#'+tabsID);
+        $(filter).find("ul li").removeClass("active");
+        $(filter)
+          .find("[data-label='" + radioInput + "']")
+          .closest("li")
+          .addClass("active");
+        $(".common > div").css("display", "none");
+
+        if (radioInput === "all-categories") {
+          $(".common > div").css("display", "flex");
+        } else {
+          $("[data-label='" + radioInput + "']").css("display", "flex");
+        }
+      }
+
+      // update the swiper
+      if (window.innerWidth < 992) {
+        initSwiperRecommendedCards();
+      }
+
+      $(".most-recommended-products .swiper-slide a").click(function (event) {
+        event.preventDefault();
+        if (window.innerWidth < 992) {
+          recommendedCardsCarousal.update();
+        }
+        return false;
+      });
+
+      // filter mobile view popup open close
+      function filtersMobileView(thisValue) {
+        if (window.innerWidth < 992) {
+          var currentElement = $(thisValue).attr("data-label");
+          if (currentElement) {
+            $(".responsive-" + currentElement).addClass("mobileViewActive");
+            $("body").addClass("freeze");
+          }
+        }
+      }
+      $(".filter-compare-sort-section a")
+        .off("click")
+        .on("click", function () {
+          filtersMobileView(this);
+        });
+
+      // to make filters popup in-active ( hide )in mobile view
+      $(".filter-close")
+        .off("click")
+        .on("click", function () {
+          $(".responsive-filters").removeClass("mobileViewActive");
+          $(".responsive-sort").removeClass("mobileViewActive");
+          $("body").removeClass("freeze");
+        });
+
+      // get the ID from URL
+      var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+          sURLVariables = sPageURL.split("&"),
+          sParameterName,
+          i;
+        for (i = 0; i < sURLVariables.length; i++) {
+          sParameterName = sURLVariables[i].split("=");
+
+          if (sParameterName[0] === sParam) {
+            return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+          }
+        }
+        return false;
+      };
+      // load the right container based on id from URL
+      var idToPass = getUrlParameter("selectedFilterID");
+      // if (idToPass !== "" && idToPass !== undefined) {
+      //   if (idToPass.includes("?")) {
+      //     $("[data-label='" + idToPass.slice(0, 1) + "']").click();
+      //   } else {
+      //     $("[data-label='" + idToPass + "']").click();
+      //   }
+      // }
     });
 
   //});
