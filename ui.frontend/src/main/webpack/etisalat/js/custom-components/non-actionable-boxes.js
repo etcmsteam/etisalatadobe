01040da48b2ab11cf7c4ac7cfe29bfbbc0ee71d2
Krishnarjuna Rao Routhu <cap77715@adobe.com>
2022-03-08 13:51:39 +0530

    feature/ETSL-1053 Added CSS and JS files

--- ui.frontend/src/main/webpack/etisalat/js/custom-components/non-actionable-boxes.js
+++ ui.frontend/src/main/webpack/etisalat/js/custom-components/non-actionable-boxes.js
@@ -24,5 +24,64 @@
         }
       });
     }
+
+    const limitText = function (title, limit) {
+      const newTitle = [];
+      const remainingTitle = [];
+      if (title.replace(/\s/g, "").length > limit) {
+        title.split(" ").reduce(function (acc, curr) {
+          if (acc + curr.length <= limit) {
+            newTitle.push(curr);
+          } else {
+            remainingTitle.push(curr);
+          }
+          return acc + curr.length;
+        }, 0);
+
+        return [newTitle.join(" "), remainingTitle.join(" ")];
+      }
+      return title;
+    };
+
+    $(".nab-content .paragraph-styling p").each(function () {
+      if ($(this).hasClass("processed") === false) {
+        var $eleText = $(this).html().trim();
+        var $textResult = limitText($eleText, 210);
+
+        if ($eleText.replace(/\s/g, "").length >= 210) {
+          var $customSpan = $("<span>");
+          $customSpan.addClass("custom-span");
+          $customSpan.html(" " + $textResult[1]);
+
+          var $customDots = $("<span>");
+          $customDots.addClass("custom-dots");
+          $customDots.html(" ...");
+
+          var paragraphwithDots = $(this).html($textResult[0]);
+          paragraphwithDots.append($($customDots));
+          paragraphwithDots.append($($customSpan));
+
+          $customSpan.css("display", "none");
+
+          $(this).closest(".clickable-area").addClass("pointer");
+          cliceableArea($(this).closest(".clickable-area"));
+        } else {
+          $(this).closest(".clickable-area").find(".action").css("display", "none");
+        }
+
+        $(this).addClass("processed");
+      }
+    });
+
+    function cliceableArea(target) {
+      $(target)
+        .off("click")
+        .on("click", function (e) {
+          e.preventDefault();
+          $(this).find(".paragraph-styling .custom-dots").toggle();
+          $(this).find(".paragraph-styling .custom-span").toggle();
+          $(this).find(".action").toggleClass("expanded");
+        });
+    }
   });
 })();
