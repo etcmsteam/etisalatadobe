31c7fd7abbd74168677b2fbcf2ba51b6d84a7811
Sai Krishna Sriram <hcl81231@adobe.com>
2022-03-10 18:52:32 +0530

    ETSL-1286 - Image Etisalat View port changes along with other component changes
    Image Etisalat View port changes along with other component changes

--- core/src/test/java/com/etisalat/core/models/ImageViewportModelTest.java
+++ core/src/test/java/com/etisalat/core/models/ImageViewportModelTest.java
@@ -41,31 +41,46 @@ public class ImageViewportModelTest {
   
   @Test
   void testImageSixViewports() {
-    String expectImage414px = "/content/dam/etisalat/offerbanner/bg-cards/414x200.jpg";
-    String expectImage768px = "/content/dam/etisalat/offerbanner/bg-cards/768x200.jpg";
-    String expectImage1024px = "/content/dam/etisalat/offerbanner/bg-cards/1024x363.jpg";
-    String expectImage1366px = "/content/dam/etisalat/offerbanner/bg-cards/1366x363.jpg";
-    String expectImage1440px = "/content/dam/etisalat/offerbanner/bg-cards/1440x363.jpg";
-    String expectImage1920px = "/content/dam/etisalat/offerbanner/bg-cards/1920x363.jpg";
-    String expectImage1920AltText = "image1920AltText";
+	String expectImage414px1x = "/content/dam/etisalat/offerbanner/bg-cards/414x200.jpg";
+	String expectImage414px2x = "/content/dam/etisalat/offerbanner/bg-cards/414x200.jpg";
+	String expectImage1024px1x = "/content/dam/etisalat/offerbanner/bg-cards/1024x768.jpg";
+	String expectImage1024px2x = "/content/dam/etisalat/offerbanner/bg-cards/1024x768.jpg";
+	String expectImage768px1x = "/content/dam/etisalat/offerbanner/bg-cards/768x200.jpg";
+	String expectImage768px2x = "/content/dam/etisalat/offerbanner/bg-cards/768x200.jpg";
+	String expectImage1440px1x = "/content/dam/etisalat/offerbanner/bg-cards/1440x363.jpg";
+	String expectImage1440px2x = "/content/dam/etisalat/offerbanner/bg-cards/1440x363.jpg";
+	String expectImage1336px1x = "/content/dam/etisalat/offerbanner/bg-cards/1336x768.jpg";
+	String expectImage1336px2x = "/content/dam/etisalat/offerbanner/bg-cards/1336x768.jpg";
+	String expectImage1920px1x = "/content/dam/etisalat/offerbanner/bg-cards/1920x1080.jpg";
+	String expectImage1920px2x = "/content/dam/etisalat/offerbanner/bg-cards/1920x1080.jpg";
 
     Resource resource = context.resourceResolver().getResource(SIX_IMAGE_VIEWPORT_DATA);
     ImageViewportModel item = resource.adaptTo(ImageViewportModel.class);
-    String actualImage1024px = item.getImage1024px();
-    String actualImage1366px = item.getImage1366px();
-    String actualImage1440px = item.getImage1440px();
-    String actualImage1920px = item.getImage1920px();
-    String actualImage414px = item.getImage414px();
-    String actualImage768px = item.getImage768px();
-    String actualImage1920AltText = item.getImage1920PXAltText();
+    String actualImage768px1x = item.getRetinaImage768px1x();
+    String actualImage768px2x = item.getRetinaImage768px2x();
+    String actualImage1440px1x = item.getRetinaImage1440px1x();
+    String actualImage1440px2x = item.getRetinaImage1440px2x();
+    String actualImage414px1x = item.getRetinaImage414px1x();
+    String actualImage414px2x = item.getRetinaImage414px2x();
+    String actualImage1024px1x = item.getRetinaImage1024px1x();
+    String actualImage1024px2x = item.getRetinaImage1024px2x();
+    String actualImage1336px1x = item.getRetinaImage1336px1x();
+    String actualImage1336px2x = item.getRetinaImage1336px2x();
+    String actualImage1920px1x = item.getRetinaImage1920px1x();
+    String actualImage1920px2x = item.getRetinaImage1920px2x();
 
-    assertEquals(expectImage414px, actualImage414px);
-    assertEquals(expectImage768px, actualImage768px);
-    assertEquals(expectImage1024px, actualImage1024px);
-    assertEquals(expectImage1366px, actualImage1366px);
-    assertEquals(expectImage1440px, actualImage1440px);
-    assertEquals(expectImage1920px, actualImage1920px);
-    assertEquals(expectImage1920AltText, actualImage1920AltText);
+    assertEquals(expectImage768px1x, actualImage768px1x);
+    assertEquals(expectImage768px2x, actualImage768px2x);
+    assertEquals(expectImage1440px1x, actualImage1440px1x);
+    assertEquals(expectImage1440px2x, actualImage1440px2x);
+    assertEquals(expectImage414px1x, actualImage414px1x);
+    assertEquals(expectImage414px2x, actualImage414px2x);
+    assertEquals(expectImage1024px1x, actualImage1024px1x);
+    assertEquals(expectImage1024px2x, actualImage1024px2x);
+    assertEquals(expectImage1336px1x, actualImage1336px1x);
+    assertEquals(expectImage1336px2x, actualImage1336px2x);
+    assertEquals(expectImage1920px1x, actualImage1920px1x);
+    assertEquals(expectImage1920px2x, actualImage1920px2x);
   }
   
   @Test
@@ -78,7 +93,6 @@ public class ImageViewportModelTest {
     String expectImage768px2x = "/content/dam/etisalat/offerbanner/bg-cards/768x200.jpg";
     String expectImage1440px1x = "/content/dam/etisalat/offerbanner/bg-cards/1440x363.jpg";
     String expectImage1440px2x = "/content/dam/etisalat/offerbanner/bg-cards/1440x363.jpg";
-    String expectImage1440AltText = "image1440AltText";
 
 
     Resource resource = context.resourceResolver().getResource(FOUR_IMAGE_VIEWPORT_DATA);
@@ -91,7 +105,6 @@ public class ImageViewportModelTest {
     String actualImage414px2x = item.getRetinaImage414px2x();
     String actualImage540px1x = item.getRetinaImage540px1x();
     String actualImage540px2x = item.getRetinaImage540px2x();
-    String actualImage1440AltText = item.getImage1440PXAltText();
 
     assertEquals(expectImage768px1x, actualImage768px1x);
     assertEquals(expectImage768px2x, actualImage768px2x);
@@ -101,7 +114,6 @@ public class ImageViewportModelTest {
     assertEquals(expectImage414px2x, actualImage414px2x);
     assertEquals(expectImage540px1x, actualImage540px1x);
     assertEquals(expectImage540px2x, actualImage540px2x);
-    assertEquals(expectImage1440AltText, actualImage1440AltText);
   }
   
   @Test
@@ -112,7 +124,6 @@ public class ImageViewportModelTest {
     String expectImage768px2x = "/content/dam/etisalat/offerbanner/bg-cards/768x200.jpg";
     String expectImage992px1x = "/content/dam/etisalat/offerbanner/bg-cards/992x200.jpg";
     String expectImage992px2x = "/content/dam/etisalat/offerbanner/bg-cards/992x200.jpg";
-    String expectImage992AltText = "image992AltText";
 
     Resource resource = context.resourceResolver().getResource(THREE_IMAGE_VIEWPORT_DATA);
     ImageViewportModel item = resource.adaptTo(ImageViewportModel.class);
@@ -122,7 +133,6 @@ public class ImageViewportModelTest {
     String actualImage414px2x = item.getRetinaImage414px2x();
     String actualImage992px1x = item.getRetinaImage992px1x();
     String actualImage992px2x = item.getRetinaImage992px2x();
-    String actualImage992AltText = item.getImage992PXAltText();
 
     assertEquals(expectImage768px1x, actualImage768px1x);
     assertEquals(expectImage768px2x, actualImage768px2x);
@@ -130,7 +140,6 @@ public class ImageViewportModelTest {
     assertEquals(expectImage414px2x, actualImage414px2x);
     assertEquals(expectImage992px1x, actualImage992px1x);
     assertEquals(expectImage992px2x, actualImage992px2x);
-    assertEquals(expectImage992AltText, actualImage992AltText);
   }
   
   @Test
