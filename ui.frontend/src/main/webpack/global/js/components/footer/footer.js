46b75ab3282243d52ab425bf49e1cd2452caabea
Krishnarjuna Rao Routhu <cap77715@adobe.com>
2022-03-16 16:03:18 +0530

    Bugfix/ETSL-1417 added javascript changes to fix footer download icons

--- ui.frontend/src/main/webpack/global/js/components/footer/footer.js
+++ ui.frontend/src/main/webpack/global/js/components/footer/footer.js
@@ -1,26 +1,32 @@
 /* eslint-disable */
 import { isMobile, IsMediaQuery } from "../../helpers";
 
-export const FOOTER_DOWNNLOAD_ICONS = () => {
-  const ANDROID = $(".footer .download-etisalat .android");
-  const APPLE = $(".footer .download-etisalat .apple");
-  const HUAWEI = $(".footer .download-etisalat .huawei");
+const FOOTER_DOWNNLOAD_ICONS = () => {
+  if ($(".footer .download-etisalat .icon").length > 0) {
+    const ANDROID = $(".android");
+    const APPLE = $(".apple");
+    const HUAWEI = $(".huawei");
 
-  if (!IsMediaQuery.lg.matches) {
-    if (isMobile.Android()) {
-      APPLE.hide();
+    if (!IsMediaQuery.lg.matches) {
+      if (isMobile.Android()) {
+        APPLE.hide();
+        ANDROID.show();
+        HUAWEI.show();
+      }
+
+      if (isMobile.iOS()) {
+        ANDROID.hide();
+        HUAWEI.hide();
+        APPLE.show();
+      }
+    } else {
       ANDROID.show();
       HUAWEI.show();
-    }
-
-    if (isMobile.iOS()) {
-      ANDROID.hide();
-      HUAWEI.hide();
       APPLE.show();
     }
-  } else {
-    ANDROID.show();
-    HUAWEI.show();
-    APPLE.show();
   }
 };
+
+$(document).ready(function () {
+  FOOTER_DOWNNLOAD_ICONS();
+});
