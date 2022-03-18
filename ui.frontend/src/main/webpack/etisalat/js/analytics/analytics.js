db69b9a4f8b7493f1f8054ed858acca01ec9fc97
Yuvarj Srinivasan <diz32873@adobe.com>
2022-03-18 17:30:51 +0530

    analytics change for link click datalayer
    analytics change for link click datalayer

--- ui.frontend/src/main/webpack/etisalat/js/analytics/analytics.js
+++ ui.frontend/src/main/webpack/etisalat/js/analytics/analytics.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 export const ANALYTICS_FILTER = (category, type, value) => {
   window.adobeDataLayer.push({
     event: "filter",
@@ -11,3 +12,39 @@ export const ANALYTICS_FILTER = (category, type, value) => {
     },
   });
 };
+
+// CTA and Hero Banner CTA Events
+$('a.btn, a.cmp-button, a.btn-text').on('click', function(){
+  let $this = $(this);
+  const ctaName = $this.text().toLowerCase();
+  const currrentURL = window.location.href;
+  const pagePathName = window.location.pathname;
+  const dataLayerPathName = pagePathName.split('.html')[0];
+  let sectionHeading = '';
+  let btnAction = '';
+
+  const pathArr = dataLayerPathName.split('/');
+  sectionHeading = pathArr.pop();
+
+  window.adobeDataLayer.push({
+    event: "linkClicked",
+    xdmActionDetails: {
+      web: {
+        webInteraction: {
+          name: ctaName,
+          URL: currrentURL,
+          type: "[other or exit or download]",
+          region: sectionHeading,
+          linkClicks: {
+            value: 1,
+          },
+        },
+      },
+      linkInfo: {
+        sectionHeading: sectionHeading,
+        action: btnAction,
+        name: ctaName,
+      },
+    },
+  });
+});
