434e848ee6e754054159a10669147da5f26628ea
Harminder Pal Singh <hcl62823@adobe.com>
2022-03-18 20:48:13 +0530

    fixes ui and swiper issue | ETSL-1471

--- ui.frontend/src/main/webpack/etisalat/js/custom-components/context-navigation.js
+++ ui.frontend/src/main/webpack/etisalat/js/custom-components/context-navigation.js
@@ -109,3 +109,72 @@ $(document).ready(function () {
     $(this).closest(".load-more").css("display", "none");
   });
 });
+
+
+
+/**
+ * 05-context-nav.js --------------------------------------------------------------- start
+ */
+ function initContextNavCarousel () {
+
+
+  $(document).find('.row-context-nav').each(function () {
+
+    var swiperSlideLength = $(this).find('.swiper-slide').length;
+    $(this).addClass('swiper-with-' + swiperSlideLength + '-slides');
+
+    swiperInit($(this).find('.context-nav-box-wrap'), {
+      loop: false,
+      autoplay: false,
+      slidesPerView: $(this).find('.swiper-slide').length,
+      simulateTouch: true,
+      breakpoints: {
+        767: {
+          slidesPerView: 'auto',
+          centeredSlides: true,
+          spaceBetweenSlides: 0
+        },
+        9999: {
+          centeredSlides: false,
+          slidesPerView: $(this).find('.swiper-slide').length
+        }
+      }
+    });
+
+    setSpacebetweenContextNavCarousel();
+  });
+}
+
+function setSpacebetweenContextNavCarousel () {
+  var spaceBetween = 0;
+
+  $(document).find('.context-nav-box-wrap').each(function () {
+    var slideTemp = $(this)[0].swiper;
+    if (slideTemp) {
+      spaceBetween = ($(this).width() - ($(this).width() / 10 * 3 * 3)) / 2;
+      slideTemp.params.spaceBetween = spaceBetween;
+      $(this).find('.swiper-wrapper').removeAttr('style');
+      $(this).find('.swiper-slide').removeAttr('style');
+      slideTemp.update(true);
+    }
+  });
+
+}
+
+$(document).ready(function () {
+  initContextNavCarousel();
+  if ($('.context-nav-box-wrap').length) {
+    $(window).on('resize', function () {
+
+      setTimeout(function () {
+        setSpacebetweenContextNavCarousel();
+      }, 500);
+
+    });
+  }
+
+});
+
+/**
+ * 05-context-nav.js --------------------------------------------------------------- end
+ */
