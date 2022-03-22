7e447271b8df11961bc37e8979a027c432d1863f
Usha Ashokanandan <hcl64613@adobe.com>
2022-03-22 14:53:09 +0530

    Resolved Conflict | ETSL1456

--- ui.frontend/src/main/webpack/etisalat/js/core-components/tabs.js
+++ ui.frontend/src/main/webpack/etisalat/js/core-components/tabs.js
@@ -1,3 +1,4 @@
+import { ANALYTICS_LINKS_TABS } from '../analytics/analytics';
 /* eslint-disable no-undef */
 export const TABS_JS = () => {
   $(".cmp-tabs__tab").click(function () {
@@ -5,5 +6,11 @@ export const TABS_JS = () => {
     const taboffset = $("#" + tabsActiveId).offset();
     const taboffsetTop = taboffset.top - 200;
     window.scrollTo(0, taboffsetTop);
+  });
+  $(".tabs li").click(function(){
+    const tabsName = $(this).text().trim();
+    const tabsURL = window.location.href;
+    const sectionHeading = document.title;
+    ANALYTICS_LINKS_TABS(tabsName, tabsURL, sectionHeading);
   });
 };
