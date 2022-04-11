80969e5aeb90c15f3c4f5c8a20f607253457ae23
Yuvarj Srinivasan <diz32873@adobe.com>
2022-04-11 23:39:05 +0530

    E1-166 form tracking analytics
    E1-166 form tracking analytics

--- ui.frontend/src/main/webpack/etisalat/js/analytics/analytics.js
+++ ui.frontend/src/main/webpack/etisalat/js/analytics/analytics.js
@@ -44,4 +44,97 @@ export const ANALYTICS_LINKS_TABS = (name,url,heading) => {
     }
     });
   };
+
+export const FORM_SUCCESS = (form, data) => {
+  const name = form.attr('name');
+  const formId = form.attr('id');
+  console.log(form);
+  if (formId === "leadOrder") {
+    window.adobeDataLayer.push({
+      event: "form success",
+      formDetails: {
+        formName: name,
+        referenceNumber: "",
+      },
+      user: {
+        accountNumber: data.accountNumber ? data.accountNumber : "",
+        name: data.contactFirstName + data.contactLastName,
+        email: data.emailAddress ? data.emailAddress : "",
+        contactNumber: data.mobileNo ? data.mobileNo : "",
+        companyName: data.companyName ? data.companyName : "",
+        requestRequirements: data.description ? data.description : "",
+      },
+      productDetails: {
+        productName: "",
+        expectedRevenue: "",
+      },
+      eventInfo: {
+        formSuccess: 1,
+      },
+    });
+  } else if (formId === "cwsNeedHelp") {
+    window.adobeDataLayer.push({
+      event: "form success",
+      formDetails: {
+        formName: name,
+        referenceNumber: "",
+      },
+      user: {
+        accountNumber: data.accountNumber ? data.accountNumber : "",
+        name: data.contactFirstName + data.contactLastName,
+        email: data.emailAddress ? data.emailAddress : "",
+        contactNumber: data.mobileNo ? data.mobileNo : "",
+        companyName: data.companyName ? data.companyName : "",
+        requestRequirements: data.description ? data.description : "",
+      },
+      productDetails: {
+        productName: "",
+        expectedRevenue: "",
+      },
+      eventInfo: {
+        formSuccess: 1,
+      },
+    });
+  } else if (formId === "newsletterSubscription") {
+    console.log('newsletter');
+    window.adobeDataLayer.push({
+      event: "form success",
+      formDetails: {
+        formName: name,
+        referenceNumber: "",
+      },
+      user: {
+        accountNumber: data.accountNumber ? data.accountNumber : "",
+        name: data.contactFirstName + data.contactLastName,
+        email: data.emailAddress ? data.emailAddress : "",
+        contactNumber: data.mobileNo ? data.mobileNo : "",
+        companyName: data.companyName ? data.companyName : "",
+        requestRequirements: data.description ? data.description : "",
+      },
+      productDetails: {
+        productName: "",
+        expectedRevenue: "",
+      },
+      eventInfo: {
+        formSuccess: 1,
+      },
+    });
+  }
+};
+
+export const FORM_ERROR = (form) => {
+  let name = form.attr('name');
+  window.adobeDataLayer.push({
+    event: "form error",
+    formDetails: {
+      formName: name,
+      stepName: "",
+    },
+    errorInfo: {
+      formError: 1,
+      errorName: "Render Error 404",
+      rootCause: "formLoadError",
+    },
+  });
+};
   
