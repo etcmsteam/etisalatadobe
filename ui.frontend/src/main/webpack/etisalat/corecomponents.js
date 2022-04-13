64d4254c4d5bd926f6463bc669ca5397b76b0048
Deepak Sharma <deepaksharma@adobe.com>
2022-04-13 09:23:02 +0530

    Removing unwanted files

--- ui.frontend/src/main/webpack/etisalat/corecomponents.js
+++ ui.frontend/src/main/webpack/etisalat/corecomponents.js
@@ -1,20 +1,18 @@
-// Javascript or Typescript
-import "./js/core-components/*.js";
-
 // Stylesheets
 import "./corecomponents.scss";
 
 // Accordion JS
 import { ACCORDION_JS } from "./js/core-components/accordion";
-
 // Language JS
 import { LANGUAGE_JS } from "./js/core-components/language";
-
 // Tabs JS
 import { TABS_JS } from "./js/core-components/tabs";
+// Carousel Swiper
+import { CAROUSEL_SWIPER } from "./js/core-components/carousel-swiper";
 
 document.addEventListener("DOMContentLoaded", () => {
   ACCORDION_JS();
   LANGUAGE_JS();
   TABS_JS();
+  CAROUSEL_SWIPER();
 });
