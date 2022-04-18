3990b475b76722edcc280cd6cb0f6904728d9211
Jatinder <jatinderbimra@gmail.com>
2022-04-18 13:55:52 +0530

    push top nav menu desktop view fixed

--- ui.frontend/src/main/webpack/global/js/components/header/mega-navigation.js
+++ ui.frontend/src/main/webpack/global/js/components/header/mega-navigation.js
@@ -1,7 +1,26 @@
+/* eslint-disable func-names */
 /* eslint-disable no-undef */
-$(document).ready(function () {
+$(function () {
+
   $(".mega-dropdown-mob a").click(function () {
     $(this).closest("ul").children(".mega-dropdown-mob-menu").slideToggle("100");
     $(this).parent().toggleClass("open");
+  });
+
+  // top push navigation
+  $(".inc-push-meu-icon a").click(function (e) {
+    e.preventDefault();
+
+    $(".push-nav-container").toggleClass("open");
+      $("body").toggleClass("push-menu-open");
+      if ($("body").hasClass("push-menu-open")) {
+        $(".en-logo-text").addClass("in-left");
+        setTimeout(function () {
+          $(".push-close").removeClass("d-none");
+        }, 100);
+      } else {
+        $(".en-logo-text").removeClass("in-left");
+        $(".push-close").addClass("d-none");
+      }
   });
 });
