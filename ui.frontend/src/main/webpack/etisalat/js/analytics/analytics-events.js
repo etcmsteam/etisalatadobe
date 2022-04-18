fe2530111fa61be35a2475d3db161357511fc87c
Yuvarj Srinivasan <diz32873@adobe.com>
2022-04-18 11:33:36 +0530

    E1-495 exit link and download cta
    E1-495 exit link and download cta

--- ui.frontend/src/main/webpack/etisalat/js/analytics/analytics-events.js
+++ ui.frontend/src/main/webpack/etisalat/js/analytics/analytics-events.js
@@ -1,17 +1,122 @@
 (function (window, document) {
 
+  if (!window.adobeDataLayer) {
+    return false;
+  }
+
+  function isValidHttpUrl(string) {
+    try {
+      return new URL(string);
+    } catch (e) {
+      return false;
+    }
+  }
+  
   // CTA and Hero Banner CTA Events
-  $("a.btn, a.cmp-button, a.btn-text, a.link, .cmp-teaser__action-link").on("click", function () { 
+  $("a").on("click", function (e) {
+    e.preventDefault();
+
     let $this = $(this);
-    const ctaName = $this.text() ? $this.text().toLowerCase().trim() : "";
+    let trgt = e.target;
+    let ctaName = "";
     const currrentURL = window.location.href;
     const pagePathName = window.location.pathname;
     const dataLayerPathName = pagePathName.split(".html")[0];
+    const currentOrigin = window.location.origin;
     let sectionHeading = "";
     let btnAction = "";
+    let totalVal = "";
+    let productCat = "";
+    let lnkRegion = "";
 
     const pathArr = dataLayerPathName.split("/");
     sectionHeading = pathArr.pop();
+
+    if (trgt.tagName.toLowerCase() === "a") {
+      ctaName = trgt.innerText ? trgt.innerText.toLowerCase().trim() : "";
+    } else if (trgt.tagName.toLowerCase() === "img") {
+      ctaName = $(trgt).attr("alt") ? $(trgt).attr("alt").toLowerCase().trim() : "";
+    } else {
+      let title = $(trgt).closest("a").attr("title");
+      ctaName = title ? title.toLowerCase().trim() : "";
+    }
+
+    if ($this.attr("href") && $this.attr("href") !== "#") {
+      const anchorOrigin = isValidHttpUrl($this.attr("href")).origin;
+      let chkLink = $this.attr("href").split('.');
+
+      if ($this.closest("body")) {
+        lnkRegion = "main";
+      } else if ($this.closest("header")) {
+        lnkRegion = "header";
+      } else if ($this.closest("footer")) {
+        let quickLinks = $this.closest(".quick-links-head");
+        if (quickLinks.length > 0 && quickLinks.find('.links-title').length > 0) {
+          btnAction = quickLinks.find('.links-title')[0].text();
+        }
+        lnkRegion = "footer";
+      }
+
+      // Exit link click tracking
+      if (anchorOrigin && currentOrigin !== anchorOrigin) {
+        window.adobeDataLayer.push({
+          event: "linkClicked",
+          xdmActionDetails: {
+            web: {
+              webInteraction: {
+                name: ctaName,
+                URL: currrentURL,
+                type: "exit",
+                region: lnkRegion,
+                linkClicks: {
+                  value: 1,
+                },
+              },
+            },
+            linkInfo: {
+              sectionHeading: sectionHeading,
+              action: btnAction,
+              name: ctaName,
+            },
+            eventInfo: {
+              exitClick: 1,
+            },
+          },
+        });
+
+        return false;
+      }
+
+      // Download link tracking
+      if (chkLink[1].toLowerCase() === "pdf") {
+        window.adobeDataLayer.push({
+          event: "linkClicked",
+          xdmActionDetails: {
+            web: {
+              webInteraction: {
+                name: ctaName,
+                URL: currrentURL,
+                type: "download",
+                region: lnkRegion,
+                linkClicks: {
+                  value: 1,
+                },
+              },
+            },
+            linkInfo: {
+              sectionHeading: sectionHeading,
+              action: btnAction,
+              name: ctaName,
+            },
+            eventInfo: {
+              downloadClick: 1,
+            },
+          },
+        });
+      }
+
+
+    }
 
     if ($this.closest(".etisalatherobanner").length > 0) {
       btnAction = $this.closest(".etisalatherobanner").find(".hero-title");
@@ -71,8 +176,35 @@
           },
         },
       });
-    } else {
-      btnAction = ctaName;
+    } else if ($this.closest(".producttile").length > 0 && $this.hasClass("cms-button")) {
+      productCat = pathArr.pop();
+
+      let btnAct = $this.closest(".tiles-box.content").find(".tiles-box-title");
+      if (btnAct.length > 0) {
+        btnAct = btnAct.text().toLowerCase().trim();
+      } else {
+        btnAct = "";
+      }
+
+      let productPriceVal = $this.closest(".tiles-box.content").find(".tiles-box-list .detail-price-new .price");
+      let currVal = $this.closest(".tiles-box.content").find(".tiles-box-list .detail-price-new small");
+      if (productPriceVal.length > 0) {
+        productPriceVal = productPriceVal.text().toLowerCase().trim();
+        currVal = currVal.text().trim();
+        totalVal = productPriceVal + " " + currVal;
+      } else {
+        productPriceVal = "";
+        currVal = "";
+        totalVal = "";
+      }
+
+      let descriptionVal = $this.closest(".tiles-box.content").find(".tiles-box-list .featureList");
+      if (descriptionVal.length > 0) {
+        descriptionVal = descriptionVal.text().toLowerCase().trim().replace(/\n|\r/g, "");
+      } else {
+        descriptionVal = "";
+      }
+
       window.adobeDataLayer.push({
         event: "linkClicked",
         xdmActionDetails: {
@@ -89,111 +221,46 @@
           },
           linkInfo: {
             sectionHeading: sectionHeading,
-            action: btnAction,
+            action: btnAct,
             name: ctaName,
           },
-        },
-      });
-    }
-  });
-
-  // Buy Now CTA click
-  $("a.cms-button").on("click", function () {
-    let $this = $(this);
-    const ctaName = $this.text() ? $this.text().toLowerCase().trim() : "";
-    const currrentURL = window.location.href;
-    const pagePathName = window.location.pathname;
-    const dataLayerPathName = pagePathName.split(".html")[0];
-    let sectionHeading = "";
-    let totalVal = "";
-    let productCat = "";
-
-    const pathArr = dataLayerPathName.split("/");
-    sectionHeading = pathArr.pop();
-
-    productCat = pathArr.pop();
-    
-    let btnAct = $this.closest(".tiles-box.content").find(".tiles-box-title h2");
-    if (btnAct.length > 0) {
-      btnAct = btnAct.text().toLowerCase().trim();
-    } else {
-      btnAct = "";
-    }
-
-    let productPriceVal = $this.closest(".tiles-box.content").find(".tiles-box-list .detail-price-new .price");
-    let currVal = $this.closest(".tiles-box.content").find(".tiles-box-list .detail-price-new small");
-    if (productPriceVal.length > 0) {
-      productPriceVal = productPriceVal.text().toLowerCase().trim();
-      currVal = currVal.text().trim();
-      totalVal = productPriceVal + " " + currVal;
-    } else {
-      productPriceVal = "";
-      currVal = "";
-      totalVal = "";
-    }
-
-    let descriptionVal = $this.closest(".tiles-box.content").find(".tiles-box-list .featureList");
-    if (descriptionVal.length > 0) {
-      descriptionVal = descriptionVal.text().toLowerCase().trim().replace(/\n|\r/g, "");
-    } else {
-      descriptionVal = "";
-    }
-
-    window.adobeDataLayer.push({
-      event: "linkClicked",
-      xdmActionDetails: {
-        web: {
-          webInteraction: {
-            name: ctaName,
-            URL: currrentURL,
-            type: "other",
-            region: "main",
-            linkClicks: {
-              value: 1,
+          product: {
+            productDetails: {
+              productName: btnAct,
+              productPrice: totalVal,
+              productType: sectionHeading,
+              productCategory: productCat,
+              productDescription: descriptionVal,
             },
           },
         },
-        linkInfo: {
-          sectionHeading: sectionHeading,
-          action: btnAct,
-          name: ctaName,
-        },
-        product: {
-          productDetails: {
-            productName: btnAct,
-            productPrice: totalVal,
-            productType: sectionHeading,
-            productCategory: productCat,
-            productDescription: descriptionVal,
+      });
+    } else if ($this.closest("header").length > 0) {
+      window.adobeDataLayer.push({
+        event: "linkClicked",
+        xdmActionDetails: {
+          web: {
+            webInteraction: {
+              name: ctaName,
+              URL: currrentURL,
+              type: "other",
+              region: "header",
+              linkClicks: {
+                value: 1,
+              },
+            },
+          },
+          linkInfo: {
+            sectionHeading: sectionHeading,
+            action: "header",
+            name: ctaName,
+          },
+          eventInfo: {
+            headerClick: 1,
           },
         },
-      },
-    });
-  });
-
-  // Footer click Events
-  $(".footer").on("click", function (e) {
-    let trgt = e.target;
-    if (trgt.tagName.toLowerCase() === "a" ||
-    trgt.tagName.toLowerCase() === "img" ||
-    $(trgt).closest("svg").length > 0) {
-      let ctaName = "";
-      if (trgt.tagName.toLowerCase() === "a") {
-        ctaName = trgt.innerText ? trgt.innerText.toLowerCase().trim() : "";
-      } else if (trgt.tagName.toLowerCase() === "img") {
-        ctaName = $(trgt).attr("alt") ? $(trgt).attr("alt").toLowerCase().trim() : "";
-      } else {
-        let title = $(trgt).closest("a").attr("title");
-        ctaName = title ? title.toLowerCase().trim() : "";
-      }
-      const currrentURL = window.location.href;
-      const pagePathName = window.location.pathname;
-      const dataLayerPathName = pagePathName.split(".html")[0];
-      let sectionHeading = "";
-
-      const pathArr = dataLayerPathName.split("/");
-      sectionHeading = pathArr.pop();
-
+      });
+    } else if ($this.closest("footer").length > 0) {
       window.adobeDataLayer.push({
         event: "linkClicked",
         xdmActionDetails: {
@@ -218,35 +285,8 @@
           },
         },
       });
-    }
-  });
-
-  $("header").on("click", function (e) {
-    let headerTrgt = e.target;
-    if (
-      headerTrgt.tagName.toLowerCase() === "a" ||
-      headerTrgt.tagName.toLowerCase() === "img" ||
-      headerTrgt.tagName.toLowerCase() === "svg" ||
-      headerTrgt.tagName.toLowerCase() === "span" ||
-      $(headerTrgt).closest("svg").length > 0
-    ) {
-      let ctaName = "";
-      if (headerTrgt.tagName.toLowerCase() === "a" || headerTrgt.tagName.toLowerCase() === "span") {
-        ctaName = headerTrgt.innerText ? headerTrgt.innerText.toLowerCase().trim() : "";
-      } else if (headerTrgt.tagName.toLowerCase() === "img") {
-        ctaName = $(headerTrgt).attr("alt") ? $(headerTrgt).attr("alt").toLowerCase().trim() : "";
-      } else {
-        let title = $(headerTrgt).closest("a").attr("title");
-        ctaName = title ? title.toLowerCase().trim() : "";
-      }
-      const currrentURL = window.location.href;
-      const pagePathName = window.location.pathname;
-      const dataLayerPathName = pagePathName.split(".html")[0];
-      let sectionHeading = "";
-
-      const pathArr = dataLayerPathName.split("/");
-      sectionHeading = pathArr.pop();
-
+    } else {
+      btnAction = ctaName;
       window.adobeDataLayer.push({
         event: "linkClicked",
         xdmActionDetails: {
@@ -255,7 +295,7 @@
               name: ctaName,
               URL: currrentURL,
               type: "other",
-              region: "header",
+              region: "main",
               linkClicks: {
                 value: 1,
               },
@@ -263,11 +303,8 @@
           },
           linkInfo: {
             sectionHeading: sectionHeading,
-            action: "header",
+            action: btnAction,
             name: ctaName,
-          },
-          eventInfo: {
-            headerClick: 1,
           },
         },
       });
