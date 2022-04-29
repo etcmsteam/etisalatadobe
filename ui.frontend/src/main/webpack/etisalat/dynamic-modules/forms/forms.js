66239b0d927e196b5dbfa948306a83639f034d19
Krishnarjuna Rao Routhu <cap77715@adobe.com>
2022-04-29 14:02:22 +0530

    feature/E1-687-SPA intelinput javascript changes

--- ui.frontend/src/main/webpack/etisalat/dynamic-modules/forms/forms.js
+++ ui.frontend/src/main/webpack/etisalat/dynamic-modules/forms/forms.js
@@ -1,19 +1,22 @@
-import 'intl-tel-input/build/js/utils';
-import '../../js/common';
-import { FORM_DEFAULTS } from './form-defaults';
-import { LEADER_FORM } from './leader-form';
-import { OMNI_LEAD_FORM } from './omniLead-form';
-import { NEWS_LETTER_SUBSCRIPTION } from './newsletter-subscription';
-import { PROXY_FORM } from './proxy-form';
-import { NEED_HELP_FORM } from './need-help-form';
-import { SMB_FORMS } from './smb-forms';
+import "intl-tel-input/build/js/utils";
+import "intl-tel-input/build/js/intlTelInput";
+import "select2/dist/js/select2.min";
+import "../../js/common";
+
+import { FORM_DEFAULTS } from "./form-defaults";
+import { LEADER_FORM } from "./leader-form";
+import { OMNI_LEAD_FORM } from "./omniLead-form";
+import { NEWS_LETTER_SUBSCRIPTION } from "./newsletter-subscription";
+import { PROXY_FORM } from "./proxy-form";
+import { NEED_HELP_FORM } from "./need-help-form";
+import { SMB_FORMS } from "./smb-forms";
 
 export const FORMS_MODULE = () => {
-    FORM_DEFAULTS();
-    LEADER_FORM();
-    OMNI_LEAD_FORM();
-    NEWS_LETTER_SUBSCRIPTION();
-    PROXY_FORM();
-    NEED_HELP_FORM();
-    SMB_FORMS();
+  FORM_DEFAULTS();
+  LEADER_FORM();
+  OMNI_LEAD_FORM();
+  NEWS_LETTER_SUBSCRIPTION();
+  PROXY_FORM();
+  NEED_HELP_FORM();
+  SMB_FORMS();
 };
