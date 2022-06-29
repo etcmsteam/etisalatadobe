4447a04e094b04df68b1405a5ce043c780b54a99
Adarsh Nanda <adarshn@adobe.com>
2022-06-29 19:42:03 +0530

    E1-1677: added thankyou js

---
+++ ui.frontend/src/main/webpack/etisalat/js/custom-components/thankyou.js
@@ -0,0 +1,15 @@
+import { getURLParameter } from '../../../global/js/utils';
+
+export const THANK_YOU = () => {
+  if ($('#thankyoumessage').length === 0) {
+    return;
+  }
+  const referenceNo = getURLParameter('referenceNo');
+  const locale = $('html')[0].lang != '' ? $('html')[0].lang.toLowerCase() : 'en';
+  const indexPageUrl = `/${locale}/smb/index.jsp`;
+  if (referenceNo) {
+    $('.referenceNoTxt').html(referenceNo);
+  } else {
+    window.location.replace(indexPageUrl);
+  }
+};
