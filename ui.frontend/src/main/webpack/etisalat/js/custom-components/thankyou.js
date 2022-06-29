d44bda74b78647f576cd7025533f34bb0dea40ff
Adarsh Nanda <adarshn@adobe.com>
2022-06-29 19:42:33 +0530

    E1-1677: change url const

--- ui.frontend/src/main/webpack/etisalat/js/custom-components/thankyou.js
+++ ui.frontend/src/main/webpack/etisalat/js/custom-components/thankyou.js
@@ -6,7 +6,7 @@ export const THANK_YOU = () => {
   }
   const referenceNo = getURLParameter('referenceNo');
   const locale = $('html')[0].lang != '' ? $('html')[0].lang.toLowerCase() : 'en';
-  const indexPageUrl = `/${locale}/smb/index.jsp`;
+  const indexPageUrl = `/${locale}/smb/index.html`;
   if (referenceNo) {
     $('.referenceNoTxt').html(referenceNo);
   } else {
