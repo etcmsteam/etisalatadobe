711017a6015d66aa38c3efbc545c732071719ef8
Yuvarj Srinivasan <diz32873@adobe.com>
2022-07-15 21:31:25 +0530

    E1-169 adobe analytics changes
    E1-169 adobe analytics changes

--- ui.frontend/src/main/webpack/etisalat/js/core-components/tabs.js
+++ ui.frontend/src/main/webpack/etisalat/js/core-components/tabs.js
@@ -7,10 +7,19 @@ export const TABS_JS = () => {
     const taboffsetTop = taboffset.top - 200;
     window.scrollTo(0, taboffsetTop);
   });
-  $(".tabs li").click(function(){
-    const tabsName = $(this).text().trim();
+  $(".tabs .cmp-tabs__tablist li").click(function(){
+    const pagePathName = window.location.pathname;
+    const dataLayerPathName = pagePathName.split('.html')[0];
+    const pathArr = dataLayerPathName.split('/');
+    const sectionHeadingTitle = $(this).closest('.tabs').find('> h3');
+    let sectionHeading = '';
+    if (sectionHeadingTitle.length > 0) {
+      sectionHeading = sectionHeadingTitle.text().toLowerCase().trim();
+    } else {
+      sectionHeading = pathArr.pop();
+    }
+    const tabsName = $(this).text().toLowerCase().trim();
     const tabsURL = window.location.href;
-    const sectionHeading = document.title;
     ANALYTICS_LINKS_TABS(tabsName, tabsURL, sectionHeading);
   });
 };
